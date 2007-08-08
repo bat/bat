4cd80378b3db5f41000c7f9bf8d6d1091e2f5c6e
Daniel Kollar <daniel.kollar@cern.ch>
2007-08-08 14:58:08 +0000

    updated mkmodelclass.sh to match the log scheme

--- tools/mkmodelclass.sh
+++ tools/mkmodelclass.sh
@@ -23,63 +23,63 @@ cat << EOF
 #ifndef __|:UP_MODEL:|__H
 #define __|:UP_MODEL:|__H
 
-#include "BCModel.h" 
+#include "BCModel.h"
 
 // This is a |:Model:| header file.
 // Model source code is located in file |:Model:|.cxx
 
-// --------------------------------------------------------- 
-class |:Model:| : public BCModel 
+// ---------------------------------------------------------
+class |:Model:| : public BCModel
 {
-  public: 
+	public:
 
-  // Coonstructors and destructor
-    |:Model:|();
-    |:Model:|(const char* name); 
-    ~|:Model:|();
+		// Coonstructors and destructor
+		|:Model:|();
+		|:Model:|(const char* name);
+		~|:Model:|();
 
 
-  // Methods to overload, see file |:Model:|.cxx
+		// Methods to overload, see file |:Model:|.cxx
 
-    void DefineParameters(); 
+		void DefineParameters();
 
-    double APrioriProbability(std::vector <double> parameters); 
+		double LogAPrioriProbability(std::vector <double> parameters);
 
-    double Likelihood(std::vector <double> parameters);
+		double LogLikelihood(std::vector <double> parameters);
 
-//    double ConditionalProbabilityEntry(BCDataPoint* datapoint, std::vector <double> parameters); 
+//		double LogConditionalProbabilityEntry(BCDataPoint* datapoint, std::vector <double> parameters);
 
-//    double PoissonProbability(int nentries, std::vector <double> parameters); 
+//		double LogPoissonProbability(int nentries, std::vector <double> parameters);
 
-}; 
-// --------------------------------------------------------- 
+};
+// ---------------------------------------------------------
 
-#endif 
+#endif
 
 EOF
 }
 
 function m_sourcecode() {
 cat << EOF
-#include "|:Model:|.h" 
+#include "|:Model:|.h"
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 |:Model:|::|:Model:|() : BCModel()
 {  // default constructor
-   DefineParameters();
+	DefineParameters();
 };
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 |:Model:|::|:Model:|(const char* name) : BCModel(name)
 {  // constructor
-   DefineParameters();
+	DefineParameters();
 };
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 |:Model:|::~|:Model:|()
 {};  // default destructor
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 void |:Model:|::DefineParameters()
 {
 // Add parameters to your model here.
@@ -89,73 +89,83 @@ void |:Model:|::DefineParameters()
 // of the parameter. The indeces increase from 0 according to the
 // order of adding the parameters.
 
-//   this -> AddParameter("par1", 0.0, 1.0);   // index 0 
-//   this -> AddParameter("par2", -7.0, 28.0); // index 1
+//	this -> AddParameter("par1", 0.0, 1.0);   // index 0 
+//	this -> AddParameter("par2", -7.0, 28.0); // index 1
 
 }
 
-// --------------------------------------------------------- 
-double |:Model:|::Likelihood(std::vector <double> parameters)
+// ---------------------------------------------------------
+double |:Model:|::LogLikelihood(std::vector <double> parameters)
 {
-// Put the code for calculating the Likelyhood p(data|params) here.
-// Alternatively you can overload the methods ConditionalProbabilityEntry
-// and PoissonProbability below and comment out this method.
-// See BC documentation for more details.
+// Put the code for calculating natural logarithm of the Likelyhood
+// p(data|params) here.
+// Alternatively you can overload the methods LogConditionalProbabilityEntry
+// and LogPoissonProbability below and comment out this method.
+// See BAT documentation for more details.
 
-// This method is called many times during integration and marginalization,                      
-// so it should be rather efficient. I.e. it should use parameters[i]                            
-// instead of calls to GetParameter("name"). The user should make sure that 
-// he calls the parameters by the index correctly.                                               
+// This method is called many times during integration and marginalization,
+// so it should be rather efficient. I.e. it should use parameters[i]
+// instead of calls to GetParameter("name"). The user should make sure that
+// he calls the parameters by the index correctly.
 
 
 // The likelihood does not have to be normalized.
 
-   double probability = 1.0; 
+	double logprob = 0.;
 
-   return probability; 
+// input the code for calculating probability here
+
+	return logprob;
 }
 
-// --------------------------------------------------------- 
-double |:Model:|::APrioriProbability(std::vector <double> parameters)
+// ---------------------------------------------------------
+double |:Model:|::LogAPrioriProbability(std::vector <double> parameters)
 {
-// Put the code for calculating the Prior probability p_0(params) here.
+// Put the code for calculating natural logarithm of the Prior probability
+// p_0(params) here.
 // The prior does not have to be normalized.
 
-   double probability = 1.0; 
+	double logprob = 0.;
 
-   return probability; 
+// input the code for calculating probability here
+
+	return logprob;
 }
 
-// ---------------------------------------------------------                                     
-// ---------------------------------------------------------                                     
-// Alternatively to overloading the Likelihood method, you can overload                          
-// the two methods below. In this case you have to comment out the                               
-// Likelihood method in this file as well as in the header file.
-// If you use these methods the likelihood is then calculated as a product                       
-// of ConditionalProbabilityEntry() for individual data points times
-// PoissonProbability(). See BC documentation for more details.    
+// ---------------------------------------------------------
+// ---------------------------------------------------------
+// Alternatively to overloading the LogLikelihood method, you can overload
+// the two methods below. In this case you have to comment out the
+// LogLikelihood method in this file as well as in the header file.
+// If you use these methods the likelihood is then calculated as a product
+// of LogConditionalProbabilityEntry() for individual data points times
+// LogPoissonProbability(). See BAT documentation for more details.
 //
-/*                                                                                               
-// --------------------------------------------------------- 
-double |:Model:|::ConditionalProbabilityEntry(BCDataPoint* datapoint,
+/*
+// ---------------------------------------------------------
+double |:Model:|::LogConditionalProbabilityEntry(BCDataPoint* datapoint,
           std::vector <double> parameters)
 {
-   double probability = 1.0; 
+	double logprob = 0.;
 
-   return probability; 
+// input the code for calculating probability here
+
+	return logprob;
 }
 
-// --------------------------------------------------------- 
-double |:Model:|::PoissonProbability(int nentries,
+// ---------------------------------------------------------
+double |:Model:|::LogPoissonProbability(int nentries,
           std::vector <double> parameters)
 {
-   double probability = 1.0; 
+	double logprob = 0.;
 
-   return probability; 
+// input the code for calculating probability here
+
+	return logprob;
 }
 */
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 
 EOF
 }
