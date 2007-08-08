4fed537a7fed583927c338c3b0863d60801766c9
Daniel Kollar <daniel.kollar@cern.ch>
2007-08-08 14:27:43 +0000

    added binomial functions and factorial functions to BCMath

--- include/BCMath.h
+++ include/BCMath.h
@@ -12,6 +12,8 @@
  * 
  * REVISION: 
  *
+ *  08.08.2007  Dano  * added functions for binomial distributions and factorials
+ *
  * --------------------------------------------------------- 
  *
  *
@@ -41,8 +43,50 @@ namespace BCMath
 	 * constant, i.e. divided by sqrt(2*Pi)*sigma.
 	 */
 	double LogGaus(double x, double mean = 0, double sigma = 1, bool norm = false);
+
+	/*
+	 * Calculates Binomial probability using approximations for factorial
+	 * calculations if calculation for number greater than 100 required
+	 * using the BCMath::ApproxLogFact function.
+	 */
+	double ApproxBinomial(int n, int k, double p);
+
+	/*
+	 * Calculates natural logarithm of the Binomial probability using
+	 * approximations for factorial calculations if calculation for number greater
+	 * than 100 required using the BCMath::ApproxLogFact function.
+	 */
+	double LogApproxBinomial(int n, int k, double p);
+
+	/*
+	 * Calculates natural logarithm of the Binomial factor "n over k" using
+	 * approximations for factorial calculations if calculation for number
+	 * greater than 100 required using the BCMath::ApproxLogFact function.
+	 * Even for large numbers the calculation is performed precisely, if
+	 * n-k < 5
+	 */
+	double LogBinomFactor(int n, int k);
+
+	/*
+	 * Calculates natural logarithm of the n-factorial (n!) using approximation
+	 * log(n!) = n*log(n) - n + .5*log(2*pi*n) -1/(12*x) if n > 100.
+	 * If n<=100 it uses BCMath::LogFact to calculate it exactly.
+	 */
+	double BCMath::ApproxLogFact(double x);
+
+	/*
+	 * Calculates natural logarithm of the Binomial factor "n over k".
+	 */
+	double LogNoverK(int n, int k);
+
+	/*
+	 * Calculates natural logarithm of the n-factorial (n!)
+	 */
+	double LogFact(int n);
+
 }; 
 
 // --------------------------------------------------------- 
 
-#endif 
+#endif
+
--- src/BCMath.cxx
+++ src/BCMath.cxx
@@ -1,7 +1,6 @@
 #include "BCMath.h"
 
 // --------------------------------------------------------- 
-
 double BCMath::LogGaus(double x, double mean, double sigma, bool norm)
 {
 	// if we have a delta function, return fixed value
@@ -25,4 +24,90 @@ double BCMath::LogGaus(double x, double mean, double sigma, bool norm)
 }
 
 // --------------------------------------------------------- 
+double BCMath::ApproxBinomial(int n, int k, double p)
+{
+	return TMath::Exp( BCMath::LogApproxBinomial(n, k, p) );
+}
+
+// --------------------------------------------------------- 
+double BCMath::LogApproxBinomial(int n, int k, double p)
+{
+	// switch parameters if n < k
+	if(n<k)
+	{
+		int a=n;
+		n=k;
+		k=a;
+	}
+
+	return BCMath::LogBinomFactor(n,k) + (double)k*TMath::Log(p) + (double)(n-k)*TMath::Log(1.-p);
+}
+
+// --------------------------------------------------------- 
+double BCMath::LogBinomFactor(int n, int k)
+{
+	// switch parameters if n < k
+	if(n<k)
+	{
+		int a=n;
+		n=k;
+		k=a;
+	}
+
+	if(n==k || k==0) return 0.;
+	if(k==1 || k==n-1) return TMath::Log((double)n);
+
+	// set treshold for using approximations
+	int flimit = 100;
+
+	// if no approximation needed
+	if(n<flimit || (n-k)<5) return BCMath::LogNoverK(n,k);
+
+	// calculate final log(n over k) using approximations if necessary
+	return BCMath::ApproxLogFact((double)n) - BCMath::ApproxLogFact((double)k) - BCMath::ApproxLogFact((double)(n-k));
+}
+
+// --------------------------------------------------------- 
+double BCMath::ApproxLogFact(double x)
+{
+	int flimit=100;
+	if(x>flimit)
+		return x*TMath::Log(x) - x + .5*TMath::Log(2.*TMath::Pi()*x) - 1./(12.*x);
+	else
+		return BCMath::LogFact((int)x);
+}
+
+// --------------------------------------------------------- 
+double BCMath::LogFact(int n)
+{
+	double ln = 0.;
+	for(int i=1;i<n;i++)
+		ln += TMath::Log((double)i);
+	return ln;
+}
+
+// --------------------------------------------------------- 
+double BCMath::LogNoverK(int n, int k)
+{
+	// switch parameters if n < k
+	if(n<k)
+	{
+		int a = n;
+		n = k;
+		k = a;
+	}
+
+	if(n==k || k==0) return 0.;
+	if(k==1 || k==n-1) return TMath::Log((double)n);
+
+	int lmax = TMath::Max(k,n-k);
+	int lmin = TMath::Min(k,n-k);
+
+	double ln = 0.;
+	for(int i=n;i>lmax;i--)
+		ln += TMath::Log((double)i);
+	ln -= BCMath::LogFact(lmin);
+
+	return ln;
+}
 
