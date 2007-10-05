cba7393057794c92ac37e968a4eb98aa19ca5ed0
Kevin Kroeninger <kevin.kroeninger@phys.uni-goettingen.de>
2007-10-05 11:36:26 +0000

    Added a directory for benchmark tests, such as integration, marginalization, mode finding, etc.

---
+++ benchmarks/integration/src/BCModelGauss.cxx
@@ -0,0 +1,63 @@
+#include "BCModelGauss.h" 
+
+#include <math.h>
+
+// --------------------------------------------------------- 
+
+BCModelGauss::BCModelGauss() : BCModel()
+{
+
+	// define parameters
+
+	this -> DefineParameters();
+
+}
+
+// --------------------------------------------------------- 
+
+BCModelGauss::BCModelGauss(const char* name) : BCModel(name)
+{
+
+	// define parameters
+
+	this -> DefineParameters();
+
+}
+
+// --------------------------------------------------------- 
+
+void BCModelGauss::DefineParameters()
+{
+
+	this -> AddParameter("x", -10.0, 10.0);  // index 0
+	this -> AddParameter("y", -10.0, 10.0);  // index 0
+	this -> AddParameter("z", -10.0, 10.0);  // index 0
+
+}
+
+// --------------------------------------------------------- 
+
+double BCModelGauss::LogAPrioriProbability(std::vector <double> parameters)
+{
+
+	return 0;
+
+}
+
+// --------------------------------------------------------- 
+
+double BCModelGauss::LogLikelihood(std::vector <double> parameters)
+{
+
+	double logprob = 0; 
+
+	logprob += BCMath::LogGaus(parameters.at(0), 0.0, 0.5, true);
+	logprob += BCMath::LogGaus(parameters.at(1), 0.0, 0.5, true);
+	logprob += BCMath::LogGaus(parameters.at(2), 0.0, 0.5, true);
+	logprob += log(5.0); 
+
+	return logprob; 
+
+}
+
+// --------------------------------------------------------- 
