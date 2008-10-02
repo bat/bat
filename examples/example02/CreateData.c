c42351f48f618877fe2816eaa49666e9720b2cf4
Daniel Kollar <daniel.kollar@cern.ch>
2008-10-02 13:50:03 +0000

    Changes in Example02: - added an error message if data file doesn't exist - replaced CreateData.c with a macro equivalent to that used in   example01 with function changed to 2nd order polynomial   (the old macro for example02 was not creating the data file) - rerun the example02 with new data file produced by default   and replaced the benchmark files
    Note:
    For both examples (01 and 02) the output files are now produced
    in the main working directory instead of the output subdir.

--- examples/example02/CreateData.c
+++ examples/example02/CreateData.c
@@ -1,151 +1,90 @@
-#include <fstream.h> 
+#include <fstream.h>
 
-void CreateData()
+/*
+ * This macro generates a dataset consisting of 3 values for
+ * each data point: x, y, error on y
+ * The data are generated according to 2nd order polynomial with
+ * gausian spread. Below you can adjust the parameters of the
+ * function (par0, par1, par2), spread of the gausian (sigmay), the
+ * range of x-values of the dataset (xmin, xmax) as well as
+ * the number of points in the dataset (npoints)
+ *
+ * The random seed for the generation (RandomSeed) of the data
+ * has to be set to 0 if you want to generate different datasets.
+ * Otherwise always the same dataset will be generated.
+ *
+ * You can invoke this macro from the command line by calling
+ *    $ root -l CreateData.c
+ * and from  within ROOT by calling
+ *    root[1] .x CreateData.c
+ * or
+ *    root[1] .L CreateData.c
+ *    root[2] CreateData()
+ *
+ * To store the data to a different file then the default,
+ * you can call
+ *    $ root -l 'CreateData.c("data01.txt")'
+ * or
+ *    root[1] .x CreateData("data01.txt")
+ * or
+ *    root [1] .L CreateData.c
+ *    root[2] CreateData("data01.txt"")
+ */
+
+void CreateData(const char * fname = "./data/data.txt")
 {
-	
-  // parameters 
-	
-  int npoints = 10; 
-  float par0; 
-  float par1; 
-  float par2; 
-	
-  float sigmay = 0.2; 
-	
-  float xmin =   0.0; 
-  float xmax = 100.0; 
-	
-  bool random = false; 
-	
-  int ndatasets = 1; 
-	
-  // initialize random number generator 
-	
-  TRandom* fRandom = new TRandom(1000); 
-	
-  // loop over polynomials 
-	
-  for (int ipol = 0; ipol < 3; ipol++)
-    {
-      if (ipol == 0) 
-				{
-					par0 = 1.0; 
-					par1 = 0.0; 
-					par2 = 0.0; 
-				}
-			
-      else if (ipol == 1)
-				{
-					par0 = 2.0; 
-					par1 = 0.01; 
-					par2 = 0.0; 
-				}
-			
-      else
-				{
-					par0 = 1.0; 
-					par1 = 0.0; 
-					par2 = 0.0003; 
-				}
-			
-      // open filelist file 
-			
-      std::fstream filelist_stream; 
-			
-      filelist_stream.open(Form("./data/filelist_pol%i.txt", ipol), std::fstream::out);
-			
-      // histogram for conditional probability 
-			
-      TCanvas* canvas = new TCanvas(); 
-			
-      TH1F* hist_pcond = new TH1F(Form("hist_pcond_pol%i", ipol), "", 100, -30.0, -15.0); 
-			
-      // loop over data sets 
-      
-      for (int idataset = 0; idataset < ndatasets; idataset++)
-				{
-					
-					// open file 
-					
-					std::fstream file_stream;
-					
-					char filename[200]; 
-					
-					if (idataset == 0) 
-						sprintf(filename, "./data/data_ModelPol%i.txt", ipol); 
-					else
-						sprintf(filename, "./data/data_ModelPol%i_%i.txt", ipol, idataset); 
-					
-					file_stream.open(filename, std::fstream::out);
-					
-					// write file to filelist 
-					
-					if (idataset > 0)
-						{
-							filelist_stream << filename; 
-							
-							if (idataset < ndatasets - 1) 
-								filelist_stream << std::endl; 
-						}
-					
-					double pcond = 1.0; 
-					
-					// loop over points 
-					
-					for (int i = 0; i < npoints; i++)
-						{
-							// get x value 
-							
-							float x; 
-							
-							if (random == true)
-								x = fRandom -> Uniform(xmin, xmax); 
-							
-							else
-								x = (xmax - xmin) / float(npoints) * (float(i) + 0.5); 
-							
-							// get y value 
-							
-							float y; 
-							
-							float mean = par0 + par1 * x + par2 * x * x; 
-							
-							y = fRandom -> Gaus(mean * 100, sigmay * 100) / 100.0; 
-							
-							// conditional probability 
-							
-							pcond *= TMath::Gaus(y * 100.0, (par0 + x * par1 + x * x * par2) * 100.0, sigmay * 100.0, true);
-							
-							// write to file 
-							
-							file_stream << x << " " << y << " " << sigmay; 
-							
-							if (i < npoints - 1)
-								file_stream << std::endl; 
-						}
-					
-					// fill histogram 
-					
-					hist_pcond -> Fill(TMath::Log10(pcond)); 
-					
-					if (idataset == 0)
-						cout << " conditional probability = " << pcond << endl; 
-					
-					// close file 
-					
-					file_stream.close(); 
-					
-				}
-      
-      // print histogram 
-      
-      canvas -> cd(); 
-      hist_pcond -> Draw(); 
-			
-      // close list file 
-			
-      filelist_stream.close(); 
-			
-    }
-  
+	// parameters
+	int npoints = 10; // number of data points
+	float par0 = 1.; // offset
+	float par1 = 0.; // slope
+	float par2 = 0.0003; // quad
+	float sigmay = .2; // uncertainty on each measurement
+
+	// range of x-values
+	float xmin = 0.;
+	float xmax = 100.;
+
+	// random seed
+	int RandomSeed = 1000;
+
+	cout <<
+		"Generating dataset function   f(x) = ax^2 + bx + c" << endl <<
+		" - Range of x-values:      " << xmin << " < x < " << xmax << endl <<
+		" - Number of data points:  " << npoints << endl <<
+		" - Parameter a:            " << par2 << endl <<
+		" - Paramerer b:            " << par1 << endl <<
+		" - Paramerer c:            " << par0 << endl <<
+		" - Uncertainty:            " << sigmay << endl;
+
+
+	// initialize random number generator
+	TRandom3 * fRandom = new TRandom3(RandomSeed);
+
+	// open file
+	fstream file_data;
+	char filename[200];
+	file_data.open(fname, std::fstream::out);
+
+	// loop over points
+	for (int i = 0; i < npoints; i++)
+	{
+		// get x value
+		float x = (xmax - xmin) / float(npoints) * (float(i) + .5);
+
+		// get y value
+		float mean = par0 + par1 * x + par2 * x * x;
+		float y = fRandom -> Gaus(mean, sigmay);
+
+		// write to file
+		file_data << x << " " << y << " " << sigmay;
+
+		if (i < npoints - 1)
+			file_data << endl;
+	}
+
+	// close file
+	file_data.close();
+
+	cout << "Data have been recorded to file  " << fname << endl;
+
 }
