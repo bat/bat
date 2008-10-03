dda5a69370862c0fd85f297ee32737ae1d6b0f9f
Daniel Kollar <daniel.kollar@cern.ch>
2008-10-03 12:54:30 +0000

    Updates in example03: - removed some trailing spaces from the code :) - added an error message if data doesn't exist - all the output is now in the main directory (just like in 01 and 02) - set number of divisions on X axis for the main data plots to avoid   the overlap ov labels and regenerated the benchmark data plots

--- examples/example03/runBATexample03.c
+++ examples/example03/runBATexample03.c
@@ -3,103 +3,104 @@
 #include <BCParameter.h>
 #include <BCModelManager.h>
 #include <BCModelOutput.h>
-#include <BCDataPoint.h> 
-#include <BCH1D.h> 
+#include <BCDataPoint.h>
+#include <BCH1D.h>
 
-#include <TCanvas.h> 
-#include <TH1D.h> 
-#include <TF1.h> 
-#include <BCLog.h> 
-#include <TLegend.h> 
+#include <TCanvas.h>
+#include <TH1D.h>
+#include <TF1.h>
+#include <BCLog.h>
+#include <TLegend.h>
 
 #include  <iostream>
 
 #include "style.c"
 
 // ---------------------------------------------------------
-  
+
 int main()
 {
 
 	// ---------------------------------------------------------
-	// set style  
+	// set style
 	// ---------------------------------------------------------
 
-	// calls a function which defines a nice style. 
-	SetStyle(); 
+	// calls a function which defines a nice style.
+	SetStyle();
 
 	// ---------------------------------------------------------
-	// open log file 
+	// open log file
 	// ---------------------------------------------------------
-
-	// opens the log file. 
-	BCLog::OpenLog("output/log.txt", BCLog::detail, BCLog::detail); 
+	BCLog::OpenLog("log.txt", BCLog::detail, BCLog::detail);
 
 	// ---------------------------------------------------------
-	// define models 
+	// define models
 	// ---------------------------------------------------------
 
 	// creates the two models to be tested. The first model describes a
 	// flat (background) spectrum while the second one describes a
 	// spectrum with a flat (background) and Gaussian (signal)
-	// contribution. The parameters are defined in the model definition. 
+	// contribution. The parameters are defined in the model definition.
 
-	BCModelBackground* fModelBackground = new BCModelBackground("background model"); 
-	BCModelSignal* fModelSignal = new BCModelSignal("signal model"); 
+	BCModelBackground * fModelBackground = new BCModelBackground("background model");
+	BCModelSignal * fModelSignal = new BCModelSignal("signal model");
 
 	// ---------------------------------------------------------
-	// define manager 
+	// define manager
 	// ---------------------------------------------------------
 
 	// defines a model manager. this manager will be used to make sure
-	// all models use the same data set. 
-	BCModelManager* fModelManager = new BCModelManager(); 
+	// all models use the same data set.
+	BCModelManager * fModelManager = new BCModelManager();
 
 	// adds all three models to the manager and passes the a priori
-	// probability to the manager. 
-	fModelManager -> AddModel(fModelBackground, 0.5); 
-	fModelManager -> AddModel(fModelSignal, 0.5); 
+	// probability to the manager.
+	fModelManager -> AddModel(fModelBackground, 0.5);
+	fModelManager -> AddModel(fModelSignal, 0.5);
 
 	// ---------------------------------------------------------
-	// model output file 
+	// model output file
 	// ---------------------------------------------------------
-	
+
 	// creates a ROOT output file which stores all the necessary
-	// information. 
-	BCModelOutput * fModelOutputBackground = new BCModelOutput(fModelBackground, "output/output_Background.root"); 
-	fModelOutputBackground -> WriteMarkovChain(true); 
+	// information.
+	BCModelOutput * fModelOutputBackground = new BCModelOutput(fModelBackground, "output_Background.root");
+	fModelOutputBackground -> WriteMarkovChain(true);
 
-	BCModelOutput * fModelOutputSignal = new BCModelOutput(fModelSignal, "output/output_Signal.root"); 
-	fModelOutputSignal -> WriteMarkovChain(true); 
+	BCModelOutput * fModelOutputSignal = new BCModelOutput(fModelSignal, "output_Signal.root");
+	fModelOutputSignal -> WriteMarkovChain(true);
 
 	// ---------------------------------------------------------
-	// read data from file 
+	// read data from file
 	// ---------------------------------------------------------
 
-	// creates a new data set 
-	BCDataSet* fDataSet = new BCDataSet(); 
+	// creates a new data set
+	BCDataSet * fDataSet = new BCDataSet();
 
 	// read data from a text file. two values per data point are
 	// defined, i.e. the energy of the bin and the number of events
 	// observed in that bin. the first two entries define the region of
-	// interest in the spectrum. 
-	if (fDataSet -> ReadDataFromFileTxt("./data/data.txt", 2) != 0) 
-		return -1; 
+	// interest in the spectrum.
+	if (fDataSet -> ReadDataFromFileTxt("./data/data.txt", 2) != 0)
+	{
+		BCLog::Out(BCLog::error,BCLog::error,"You should run CreateData.c macro in ROOT to create the data file.");
+		return -1;
+	}
 
 	// assigns the data set to the model manager. the manager
 	// automatically assigns the same data set to all three models.
 	fModelManager -> SetDataSet(fDataSet);
 
 	// ---------------------------------------------------------
-	// prepare calculation of error band 
+	// prepare calculation of error band
 	// ---------------------------------------------------------
 
-	// set limits on data values 
-	fModelManager -> SetDataBoundaries(0, fDataSet -> GetDataPoint(0) -> GetValue(0), fDataSet -> GetDataPoint(0) -> GetValue(1)); 
-	fModelManager -> SetDataBoundaries(1, 0.0, 20); 
+	// set limits on data values
+	fModelManager -> SetDataBoundaries(0, fDataSet -> GetDataPoint(0) -> GetValue(0), fDataSet -> GetDataPoint(0) -> GetValue(1));
+	fModelManager -> SetDataBoundaries(1, 0., 20);
 
-	// set x and y value indices 
-	fModelManager -> SetFitFunctionIndices(0, 1); 
+	// set x and y value indices
+	fModelManager -> SetFitFunctionIndices(0, 1);
 
 	// ---------------------------------------------------------
 	// initialize
@@ -109,182 +110,180 @@ int main()
 	// normalizations for each model and calculates the model a
 	// posteriori probabilities. this step might take a while, depending
 	// on the number of parameters.
-	fModelManager -> SetIntegrationMethod(BCIntegrate::kIntCuba); 
+	fModelManager -> SetIntegrationMethod(BCIntegrate::kIntCuba);
 
 	fModelManager -> Normalize();
 
 	// ---------------------------------------------------------
-	// find mode 
+	// find mode
 	// ---------------------------------------------------------
 
-	// finds mode for all models 
-	fModelManager -> SetOptimizationMethod(BCIntegrate::kOptMinuit); 
+	// finds mode for all models
+	fModelManager -> SetOptimizationMethod(BCIntegrate::kOptMinuit);
 
-	fModelManager -> FindMode(); 
+	fModelManager -> FindMode();
 
 	// ---------------------------------------------------------
-	// marginalize 
+	// marginalize
 	// ---------------------------------------------------------
 
 	// selects the most probable model and marginalizes the probability
 	// density with respect to all parameters
 
-	// first, remember the model a posterior probabilities. 
-	double post_modelbkg = fModelBackground -> GetModelAPosterioriProbability(); 
-	double post_modelsgn = fModelSignal -> GetModelAPosterioriProbability(); 
+	// first, remember the model a posterior probabilities.
+	double post_modelbkg = fModelBackground -> GetModelAPosterioriProbability();
+	double post_modelsgn = fModelSignal -> GetModelAPosterioriProbability();
 
-	// marginalize background model 
-	
+	// marginalize background model
+
 	// marginalize with respect to all (one) parameter.  the
-	// number of bins define the numerical precision. 
+	// number of bins define the numerical precision.
 	fModelBackground -> MarginalizeAll();
 
 	// the one-dimensional marginalized probability densities are kept
 	// in memory and are returned from the model class. they are printed
-	// into a .ps file. 
-	fModelBackground -> PrintAllMarginalized("output/plots_background.ps"); 
+	// into a .ps file.
+	fModelBackground -> PrintAllMarginalized("plots_background.ps");
 
-	// marginalize signal model 
+	// marginalize signal model
 
 	// marginalize with respect to all parameters.  the
-	// number of bins define the numerical precision. 
+	// number of bins define the numerical precision.
 	fModelSignal -> MarginalizeAll();
 
 	// the one-dimensional marginalized probability densities are kept
 	// in memory and are returned from the model class. they are printed
-	// into a .ps file. 
+	// into a .ps file.
 
-	fModelSignal -> PrintAllMarginalized("output/plots_signal.ps"); 
-			
+	fModelSignal -> PrintAllMarginalized("plots_signal.ps");
+
 	// ---------------------------------------------------------
-	// write to output file 
+	// write to output file
 	// ---------------------------------------------------------
 
-	// fill the ROOT file with the actual output of the model. 
-	fModelOutputBackground -> FillAnalysisTree(); 
-	fModelOutputBackground -> WriteMarginalizedDistributions(); 
+	// fill the ROOT file with the actual output of the model.
+	fModelOutputBackground -> FillAnalysisTree();
+	fModelOutputBackground -> WriteMarginalizedDistributions();
 
-	fModelOutputSignal -> FillAnalysisTree(); 
-	fModelOutputSignal -> WriteMarginalizedDistributions(); 
+	fModelOutputSignal -> FillAnalysisTree();
+	fModelOutputSignal -> WriteMarginalizedDistributions();
 
-	// write to file and close 
-	fModelOutputBackground -> Close(); 
-	fModelOutputSignal -> Close(); 
+	// write to file and close
+	fModelOutputBackground -> Close();
+	fModelOutputSignal -> Close();
 
- // ---------------------------------------------------------
- // summarize
- // ---------------------------------------------------------
+	// ---------------------------------------------------------
+	// summarize
+	// ---------------------------------------------------------
 
-	fModelManager -> PrintSummary(); 
-	fModelManager -> PrintSummary("output/summary.txt"); 
+	fModelManager -> PrintSummary();
+	fModelManager -> PrintSummary("summary.txt");
 
- // ---------------------------------------------------------
- // Print data with best fit result 
- // ---------------------------------------------------------
+	// ---------------------------------------------------------
+	// Print data with best fit result
+	// ---------------------------------------------------------
 
-	// defines a new canvas 	
-	TCanvas* canvas_summary = new TCanvas(); 
-	canvas_summary -> cd(); 
+	// defines a new canvas
+	TCanvas * canvas_summary = new TCanvas();
+	canvas_summary -> cd();
 
-	// defines the spectrum histogram 
-	int Nbins = int(fModelManager -> GetNDataPoints() - 1); 
-	double Emin  = fModelManager -> GetDataPoint(0) -> GetValue(0); 
-	double Emax  = fModelManager -> GetDataPoint(0) -> GetValue(1); 
-	double dE    = (Emax - Emin) / double(Nbins); 
+	// defines the spectrum histogram
+	int Nbins = int(fModelManager -> GetNDataPoints() - 1);
+	double Emin  = fModelManager -> GetDataPoint(0) -> GetValue(0);
+	double Emax  = fModelManager -> GetDataPoint(0) -> GetValue(1);
+	double dE    = (Emax - Emin) / double(Nbins);
 
-	TH1D* hist_data = new TH1D("hist_data", "", Nbins, Emin, Emax); 
-	hist_data -> SetXTitle("E"); 
-	hist_data -> SetYTitle("N"); 
-	hist_data -> SetStats(false); 
+	TH1D * hist_data = new TH1D("hist_data", ";E;N", Nbins, Emin, Emax);
+	hist_data -> SetStats(false);
+	hist_data -> GetXaxis() -> SetNdivisions(505);
 
 	// sets the points of the graph to the data points read in
-	// previously. loops over all entries. 
+	// previously. loops over all entries.
 	for (int i = 1; i < fModelManager -> GetNDataPoints(); i++)
-		hist_data -> Fill(fModelManager -> GetDataPoint(i) -> GetValue(0), 
-											fModelManager -> GetDataPoint(i) -> GetValue(1)); 
+		hist_data -> Fill(fModelManager -> GetDataPoint(i) -> GetValue(0),
+				fModelManager -> GetDataPoint(i) -> GetValue(1));
 
-	// draw the spectrum 
-	hist_data -> Draw(); 
-
-	// define a constant function with the best fit parameters of the
-	// background model.
-	TF1* func_bkg = new TF1("func_bkg", "[0]", Emin, Emax); 
-	func_bkg -> SetLineWidth(3); 
-	func_bkg -> SetLineColor(kRed); 
-
-	func_bkg -> SetParameter(0, fModelBackground -> GetBestFitParameter(0) / (double(Nbins))); 
+	// draw the spectrum
+	hist_data -> Draw();
 
 	// define a constant function with the best fit parameters of the
 	// background model.
+	TF1 * func_bkg = new TF1("func_bkg", "[0]", Emin, Emax);
+	func_bkg -> SetLineWidth(3);
+	func_bkg -> SetLineColor(kRed);
 
-	TF1* func_sgn = new TF1("func_sgn", 
-													"[0] + [1]/(sqrt(2*3.1416) * [2])*exp(-0.5*((x-[3])/[2])**2)", Emin, Emax); 
-	func_sgn -> SetLineWidth(3); 
-	func_sgn -> SetLineColor(kGreen); 
+	func_bkg -> SetParameter(0, fModelBackground -> GetBestFitParameter(0) / (double(Nbins)));
 
-	func_sgn -> SetParameter(0, fModelSignal -> GetBestFitParameter(0) / double(Nbins)); 
-	func_sgn -> SetParameter(1, fModelSignal -> GetBestFitParameter(1) * dE); 
-	func_sgn -> SetParameter(2, 2.0); 
-	func_sgn -> SetParameter(3, 2039.0); 
+	// define a constant function with the best fit parameters of the
+	// background model.
 
-	// draws the functions 
-	func_bkg -> Draw("SAME"); 
-	func_sgn -> Draw("SAME"); 
+	TF1 * func_sgn = new TF1("func_sgn",
+			"[0] + [1]/(sqrt(2*3.1416) * [2])*exp(-0.5*((x-[3])/[2])**2)", Emin, Emax);
+	func_sgn -> SetLineWidth(3);
+	func_sgn -> SetLineColor(kGreen);
 
-	// define and draw a legend. 
-	TLegend* legend = new TLegend(0.65, 0.70, 0.90, 0.90); 
-	legend -> SetFillColor(kWhite); 
-	legend -> SetBorderSize(0); 
-	legend -> AddEntry(hist_data, "Data", "H"); 
-	legend -> AddEntry(func_bkg, "Background", "L"); 
-	legend -> AddEntry(func_sgn, "Signal", "L"); 
-	legend -> Draw("SAME"); 
+	func_sgn -> SetParameter(0, fModelSignal -> GetBestFitParameter(0) / double(Nbins));
+	func_sgn -> SetParameter(1, fModelSignal -> GetBestFitParameter(1) * dE);
+	func_sgn -> SetParameter(2, 2.);
+	func_sgn -> SetParameter(3, 2039.);
 
-	// print the canvas to a .ps file 
-	canvas_summary -> Print("output/data.ps"); 
+	// draws the functions
+	func_bkg -> Draw("SAME");
+	func_sgn -> Draw("SAME");
 
-	// defines a new canvas 
-	TCanvas* canvas_bestfit = new TCanvas(); 
-	canvas_bestfit -> cd(); 
+	// define and draw a legend.
+	TLegend * legend = new TLegend(0.65, 0.70, 0.90, 0.90);
+	legend -> SetFillColor(kWhite);
+	legend -> SetBorderSize(0);
+	legend -> AddEntry(hist_data, "Data", "H");
+	legend -> AddEntry(func_bkg, "Background", "L");
+	legend -> AddEntry(func_sgn, "Signal", "L");
+	legend -> Draw("SAME");
 
-	// draw data 
-	hist_data -> Draw(); 
+	// print the canvas to a .ps file
+	canvas_summary -> Print("data.ps");
 
-	// draw error band and best fit function of best model 
+	// defines a new canvas
+	TCanvas * canvas_bestfit = new TCanvas();
+	canvas_bestfit -> cd();
+
+	// draw data
+	hist_data -> Draw();
+
+	// draw error band and best fit function of best model
 	if (post_modelbkg > post_modelsgn)
-		{
-			hist_data -> Draw();
-			fModelBackground -> GetErrorBandGraph(0.16, 0.84) -> Draw("F"); 
-			func_bkg -> SetLineColor(kBlack); 
-			func_bkg -> Draw("SAME"); 
-			//			fModelPol0 -> GetErrorBandXY() -> Draw("COL");	
-			//			graph_bestfit_pol0 -> SetLineColor(kBlack); 
-			//			graph_bestfit_pol0 -> Draw("SAMEC"); 
-		}
+	{
+		hist_data -> Draw();
+		fModelBackground -> GetErrorBandGraph(0.16, 0.84) -> Draw("F");
+		func_bkg -> SetLineColor(kBlack);
+		func_bkg -> Draw("SAME");
+//		fModelPol0 -> GetErrorBandXY() -> Draw("COL");
+//		graph_bestfit_pol0 -> SetLineColor(kBlack);
+//		graph_bestfit_pol0 -> Draw("SAMEC");
+	}
 
 	if (post_modelsgn > post_modelbkg)
-		{
-			hist_data -> Draw();
-			fModelSignal -> GetErrorBandGraph(0.16, 0.84) -> Draw("F"); 
-			func_sgn -> SetLineColor(kBlack); 
-			func_sgn -> Draw("SAME"); 
-			//			fModelPol0 -> GetErrorBandXY() -> Draw("COL");	
-			//			graph_bestfit_pol0 -> SetLineColor(kBlack); 
-			//			graph_bestfit_pol0 -> Draw("SAMEC"); 
-		}
+	{
+		hist_data -> Draw();
+		fModelSignal -> GetErrorBandGraph(0.16, 0.84) -> Draw("F");
+		func_sgn -> SetLineColor(kBlack);
+		func_sgn -> Draw("SAME");
+//		fModelPol0 -> GetErrorBandXY() -> Draw("COL");
+//		graph_bestfit_pol0 -> SetLineColor(kBlack);
+//		graph_bestfit_pol0 -> Draw("SAMEC");
+	}
 
-	// draw data 
-	hist_data -> Draw("SAME"); 
+	// draw data
+	hist_data -> Draw("SAME");
 
-	// print the canvas to a .ps file 
-	canvas_bestfit -> Print("output/data_bestfit.ps"); 
-	
- // close log file 
- BCLog::CloseLog(); 
+	// print the canvas to a .ps file
+	canvas_bestfit -> Print("data_bestfit.ps");
 
- return 0; 
+	// close log file
+	BCLog::CloseLog();
+
+	return 0;
 
 }
 
 // ---------------------------------------------------------
-  
