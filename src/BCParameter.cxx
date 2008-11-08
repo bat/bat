7aade5bfe98c861b88baed96cdd4e929adcc079e
Daniel Kollar <daniel.kollar@cern.ch>
2008-11-08 22:22:54 +0000

    Space cleanup.

--- src/BCParameter.cxx
+++ src/BCParameter.cxx
@@ -1,75 +1,64 @@
-/*    
- * Copyright (C) 2008, Daniel Kollar and Kevin Kroeninger.    
- * All rights reserved.    
- *    
- * For the licensing terms see doc/COPYING.    
- */    
-  
-// ---------------------------------------------------------   
- 
+/*
+ * Copyright (C) 2008, Daniel Kollar and Kevin Kroeninger.
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+// ---------------------------------------------------------
+
 #include <iostream>
 #include <fstream>
 
-#include "BCParameter.h" 
+#include "BCParameter.h"
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 
 BCParameter::BCParameter()
 {
-
-	fName       = "parameter"; 
-	fLowerLimit = 0.0; 
-	fUpperLimit = 1.0; 
+	fName       = "parameter";
+	fLowerLimit = 0.0;
+	fUpperLimit = 1.0;
 	fNuisance   = 0;
-
 }
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 
 BCParameter::BCParameter(const char * name, double lowerlimit, double upperlimit)
 {
+	BCParameter();
 
-	BCParameter(); 
-
-	fName       = name; 
-	fLowerLimit = lowerlimit; 
-	fUpperLimit = upperlimit; 
-
+	fName       = name;
+	fLowerLimit = lowerlimit;
+	fUpperLimit = upperlimit;
 }
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 
 BCParameter::BCParameter(const BCParameter & parameter)
 {
-
-	parameter.Copy(*this); 
-
+	parameter.Copy(*this);
 }
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 
 BCParameter & BCParameter::operator = (const BCParameter & parameter)
 {
+	if (this != &parameter)
+		parameter.Copy(* this);
 
-	if (this != &parameter) 
-		parameter.Copy(* this); 
-
-	return * this; 
-
+	return * this;
 }
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 
 BCParameter::~BCParameter()
-{
+{}
 
-}
-
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 
 void BCParameter::PrintSummary()
 {
-
 	std::cout
 		<<"       > Parameter   : "<< fName << std::endl
 		<<"         Index       : "<< fIndex << std::endl
@@ -78,40 +67,36 @@ void BCParameter::PrintSummary()
 		<<"         Nuisance    : ";
 
 	if(fNuisance)
-		std::cout<<"Yes"<<std::endl; 
+		std::cout<<"Yes"<<std::endl;
 	else
-		std::cout<<"No"<<std::endl; 
+		std::cout<<"No"<<std::endl;
 
 	std::cout << std::endl;
-
 }
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 
-void BCParameter::Copy(BCParameter & parameter) const 
+void BCParameter::Copy(BCParameter & parameter) const
 {
-
-	parameter.fName       = this -> fName; 
-	parameter.fIndex      = this -> fIndex; 
-	parameter.fLowerLimit = this -> fLowerLimit;  
-	parameter.fUpperLimit = this -> fUpperLimit; 
+	parameter.fName       = this -> fName;
+	parameter.fIndex      = this -> fIndex;
+	parameter.fLowerLimit = this -> fLowerLimit;
+	parameter.fUpperLimit = this -> fUpperLimit;
 	parameter.fNuisance   = this -> fNuisance;
-
 }
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
 
 bool BCParameter::IsAtLimit(double value)
 {
-
 	if (fLowerLimit == fUpperLimit)
-		return false; 
+		return false;
 
-	if ( ( (value-fLowerLimit)*(value-fLowerLimit)/fLowerLimit/fLowerLimit <= 1e-10) || 
-			 ( (value-fUpperLimit)*(value-fUpperLimit)/fUpperLimit/fUpperLimit <= 1e-10))
-		return true; 
+	if ( ( (value-fLowerLimit)*(value-fLowerLimit)/fLowerLimit/fLowerLimit <= 1e-10) ||
+			( (value-fUpperLimit)*(value-fUpperLimit)/fUpperLimit/fUpperLimit <= 1e-10))
+		return true;
 	else
-		return false; 
+		return false;
 }
 
-// --------------------------------------------------------- 
+// ---------------------------------------------------------
