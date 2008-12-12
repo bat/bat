21dbb7cc490ac24221ac7112557dc68c8cdbc48a
Daniel Kollar <daniel.kollar@cern.ch>
2008-12-12 14:35:58 +0000

    Now added BCGraphFitter which was forgotten in the previous update

---
+++ include/BCGraphFitter.h
@@ -0,0 +1,137 @@
+/*!
+ * \class BCGraphFitter
+ * \brief A class for fitting graphs with functions
+ * \author Daniel Kollar
+ * \author Kevin Kr&ouml;ninger
+ * \version 1.0
+ * \date 2008
+ * \detail This class allows fitting of a TGraphErrors using
+ * a TF1 function. It doeasn't take the x uncertainties into account.
+ * For that look at BCGraphXFitter (not yet implemented).
+ */
+
+/*
+ * Copyright (C) 2008, Daniel Kollar and Kevin Kroeninger.
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+// ---------------------------------------------------------
+
+#ifndef __BCMODELGRAPHFITTER__H
+#define __BCMODELGRAPHFITTER__H
+
+#include <vector>
+
+#include "BCModel.h"
+
+class TGraphErrors;
+class TF1;
+
+// ---------------------------------------------------------
+
+class BCGraphFitter : public BCModel
+{
+	public:
+
+		/** \name Constructors and destructors */
+		/* @{ */
+
+		/**
+		 * Default constructor */
+		BCGraphFitter();
+
+		/**
+		 * Constructor
+		 * @param graph pointer to TGraphErrors
+		 * @param func pointer to TF1 */
+		BCGraphFitter(TGraphErrors * graph, TF1 * func);
+
+		/**
+		 * The default destructor. */
+		~BCGraphFitter();
+
+		/* @} */
+
+		/** \name Member functions (get) */
+		/* @{ */
+
+		/**
+		 * @return pointer to TGraphErrors */
+		TGraphErrors * GetGraph()
+			{ return fGraph; };
+
+		/**
+		 * @return pointer to TF1 */
+		TF1 * GetFitFunction()
+			{ return fFitFunction; };
+
+		/* @} */
+
+		/** \name Member functions (set) */
+		/* @{ */
+
+		/**
+		 * @param graph pointer to TGraphErrors object */
+		int SetGraph(TGraphErrors * graph);
+
+		/**
+		 * @param func pointer to TF1 object */
+		int SetFitFunction(TF1 * func);
+
+		/* @} */
+		/** \name Member functions (miscellaneous methods) */
+		/* @{ */
+
+		/**
+		 * The log of the prior probability. It is set to be flat in all parameters.
+		 * @param parameters vector containing the parameter values */
+		double LogAPrioriProbability(std::vector <double> parameters);
+
+		/**
+		 * The log of the conditional probability.
+		 * @param parameters vector containing the parameter values */
+		double LogLikelihood(std::vector <double> parameters);
+
+		/**
+		 * Returns the value of the 1D fit function for a given set of parameters
+		 * at a given x.
+		 * @param x point to calculate the function value at
+		 * @param parameters parameters of the function */
+		double FitFunction(std::vector <double> x, std::vector <double> parameters);
+
+		/**
+		 * Performs the fit. The graph and the function has to beset beforehand.
+		 * @return An error code. */
+		int Fit()
+			{ return this -> Fit(fGraph, fFitFunction); };
+
+		/**
+		 * Performs the fit of the graph with the function.
+		 * @param graph pointer to TGraphErrors object
+		 * @param func pointer to TF1 object
+		 * @return An error code. */
+		int Fit(TGraphErrors * graph, TF1 * func);
+
+		/**
+		 * Draw the fit in the current pad. */
+		void DrawFit(const char * options = "");
+
+		/* @} */
+
+	private:
+
+		/**
+		 * The graph containing the data. */
+		TGraphErrors * fGraph;
+
+		/**
+		 * The fit function */
+		TF1 * fFitFunction;
+
+};
+
+// ---------------------------------------------------------
+
+#endif
---
+++ src/BCGraphFitter.cxx
@@ -0,0 +1,293 @@
+/*
+ * Copyright (C) 2008, Daniel Kollar and Kevin Kroeninger.
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+// ---------------------------------------------------------
+
+//#include <iostream>
+//#include <fstream>
+
+#include <TGraphErrors.h>
+#include <TF1.h>
+#include <TString.h>
+#include <TROOT.h>
+
+#include "BCGraphFitter.h"
+
+// ---------------------------------------------------------
+
+BCGraphFitter::BCGraphFitter() : BCModel("GraphFitter")
+{
+	fGraph = 0;
+	fFitFunction = 0;
+
+	this -> MCMCSetNIterationsRun(2000);
+
+	this -> SetFillErrorBand();
+}
+
+// ---------------------------------------------------------
+
+BCGraphFitter::BCGraphFitter(TGraphErrors * graph, TF1 * func) : BCModel("GraphFitter")
+{
+	fGraph = 0;
+	fFitFunction = 0;
+
+	this -> MCMCSetNIterationsRun(2000);
+
+	this -> SetGraph(graph);
+	this -> SetFitFunction(func);
+
+	this -> SetFillErrorBand();
+}
+
+// ---------------------------------------------------------
+
+int BCGraphFitter::SetGraph(TGraphErrors * graph)
+{
+	if(!graph)
+	{
+		BCLog::Out(BCLog::error,BCLog::error,"BCGraphFitter::SetGraph() : TGraphErrors not created.");
+		return 0;
+	}
+
+	int npoints = graph -> GetN();
+	if(!npoints)
+	{
+		BCLog::Out(BCLog::error,BCLog::error,"BCGraphFitter::SetGraph() : TGraphErrors is empty.");
+		return 0;
+	}
+	else if(npoints==1)
+	{
+		BCLog::Out(BCLog::error,BCLog::error,"BCGraphFitter::SetGraph() : TGraphErrors has only one point. Not able to fit.");
+		return 0;
+	}
+	else if(npoints==2)
+	{
+		BCLog::Out(BCLog::error,BCLog::error,"BCGraphFitter::SetGraph() : TGraphErrors has only two points. Fit has no meaning.");
+		return 0;
+	}
+
+	fGraph = graph;
+
+	double * x = fGraph -> GetX();
+	double * y = fGraph -> GetY();
+	double * ex = fGraph -> GetEX();
+	double * ey = fGraph -> GetEY();
+
+	if(!ey)
+	{
+		BCLog::Out(BCLog::error,BCLog::error,"BCGraphFitter::SetGraph() : TGraphErrors has no errors set on Y. Not able to fit.");
+		return 0;
+	}
+
+	BCDataSet * ds = new BCDataSet();
+
+	// fill the dataset
+	// find x and y boundaries for the error band calculation
+	double xmin=x[0];
+	double xmax=x[0];
+	double ymin=y[0];
+	double ymax=y[0];
+	for (int i = 0; i < npoints; ++i)
+	{
+		// if x errors are not set, set them to zero
+		double errx = ex ? ex[i] : 0.;
+
+		// create the data point
+		BCDataPoint * dp = new BCDataPoint(4);
+		dp -> SetValue(0, x[i]);
+		dp -> SetValue(1, y[i]);
+		dp -> SetValue(2, errx);
+		dp -> SetValue(3, ey[i]);
+		ds -> AddDataPoint(dp);
+
+		if(x[i]-errx < xmin)
+			xmin = x[i]-errx;
+		else if(x[i]+errx > xmax)
+			xmax = x[i]+errx;
+
+		if(y[i] - 5.*ey[i] < ymin)
+			ymin = y[i] - 5.*ey[i];
+		else if(y[i] + 5.*ey[i] > ymax)
+			ymax = y[i] + 5.*ey[i];
+	}
+
+	this -> SetDataSet(ds);
+
+	// set boundaries for the error band calculation
+	this -> SetDataBoundaries(0, xmin, xmax);
+	this -> SetDataBoundaries(1, ymin, ymax);
+
+	this -> SetFitFunctionIndices(0, 1);
+
+	return this -> GetNDataPoints();
+}
+
+// ---------------------------------------------------------
+
+int BCGraphFitter::SetFitFunction(TF1 * func)
+{
+	if(!func)
+	{
+		BCLog::Out(BCLog::error,BCLog::error,"BCGraphFitter::SetFitFunction() : TF1 not created.");
+		return 0;
+	}
+
+	// get the new number of parameters
+	int npar = func -> GetNpar();
+	if(!npar)
+	{
+		BCLog::Out(BCLog::error,BCLog::error,"BCGraphFitter::SetFitFunction() : TF1 has zero parameters. Not able to fit.");
+		return 0;
+	}
+
+	// set the function
+	fFitFunction = func;
+
+	// update the model name to contain the function name
+	this -> SetName(TString::Format("GraphFitter with %s",fFitFunction->GetName()));
+
+	// reset parameters
+	fParameterSet -> clear();
+
+	// add parameters
+	for (int i = 0; i < npar; ++i)
+	{
+		double xmin;
+		double xmax;
+		fFitFunction -> GetParLimits(i, xmin, xmax);
+
+		this -> AddParameter(fFitFunction->GetParName(i), xmin, xmax);
+	}
+
+	return this -> GetNParameters();
+}
+
+// ---------------------------------------------------------
+
+BCGraphFitter::~BCGraphFitter()
+{}
+
+// ---------------------------------------------------------
+
+double BCGraphFitter::LogAPrioriProbability(std::vector <double> parameters)
+{
+	// using flat probability in all parameters
+	return 0.;
+}
+
+// ---------------------------------------------------------
+
+double BCGraphFitter::LogLikelihood(std::vector <double> params)
+{
+	// initialize probability
+	double logl = 0.;
+
+	// set the parameters of the function
+	// passing the pointer to first element of the vector is
+	// not completely safe as there might be an implementation where
+	// the vector elements are not stored consecutively in memory.
+	// however it is much faster than copying the contents, especially
+	// for large number of parameters
+	fFitFunction -> SetParameters(&params[0]);
+
+	// loop over all data points
+	for (int i = 0; i < this -> GetNDataPoints(); i++)
+	{
+		std::vector <double> x = GetDataPoint(i) -> GetValues();
+
+		// her we ignore the errors on x even when they're available
+		// i.e. we treat them just as the region specifiers
+		double y = x[1];
+		double yerr = x[3];
+		double yexp = this -> FitFunction(x,params);
+
+		// calculate log of probability assuming
+		// a Gaussian distribution for each point
+		logl += BCMath::LogGaus(y, yexp, yerr, true);
+	}
+
+	return logl;
+}
+
+// ---------------------------------------------------------
+
+double BCGraphFitter::FitFunction(std::vector <double> x, std::vector <double> params)
+{
+	// set the parameters of the function
+	fFitFunction -> SetParameters(&params[0]);
+
+	return fFitFunction -> Eval(x[0]);
+}
+
+// ---------------------------------------------------------
+
+int BCGraphFitter::Fit(TGraphErrors * graph, TF1 * func)
+{
+	// set graph
+	if (!this -> SetGraph(graph))
+		return 0;
+
+	// set function
+	if (!this -> SetFitFunction(func))
+		return 0;
+
+	// perform marginalization
+	this -> MarginalizeAll();
+
+	// maximize posterior probability, using the best-fit values close
+	// to the global maximum from the MCMC
+	this -> FindModeMinuit( this -> GetBestFitParameters() );
+
+	// calculate p-value from the chi2 probability
+	// this is only valid for a product of gaussiang which is the case for
+	// the BCGraphFitter
+	this -> GetPvalueFromChi2(this -> GetBestFitParameters(), 4);
+
+	return 1;
+}
+
+// ---------------------------------------------------------
+
+void BCGraphFitter::DrawFit(const char * options)
+{
+	if (!fGraph)
+	{
+		BCLog::Out(BCLog::error, BCLog::error,"BCGraphFitter::DrawFit() : TGraphErrors not defined.");
+		return;
+	}
+
+	if (!fFitFunction)
+	{
+		BCLog::Out(BCLog::error, BCLog::error,"BCGraphFitter::DrawFit() : Fit function not defined.");
+		return;
+	}
+
+	// check wheather options contain "same"
+	TString opt = options;
+	opt.ToLower();
+
+	// if not same, draw the histogram first to get the axes
+	if(!opt.Contains("same"))
+		fGraph -> Draw("ap");
+
+	// draw the error band as central 68% probability interval
+	this -> GetErrorBandGraph(0.16, 0.84) -> Draw("f same");
+
+	// now draw the histogram again since it was covered by the band
+	fGraph -> Draw("p same");
+
+	// draw the fit function on top
+	TGraph * gfit = this -> GetFitFunctionGraph( this->GetBestFitParameters() );
+	gfit -> SetLineColor(kRed);
+	gfit -> SetLineWidth(2);
+	gfit -> Draw("l same");
+
+	gPad -> RedrawAxis();
+}
+
+// ---------------------------------------------------------
