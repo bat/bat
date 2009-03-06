ace8f9751e4154f53c14407fade48ff8d2c2e60f
Daniel Kollar <daniel.kollar@cern.ch>
2009-03-06 21:47:37 +0000

    updated CreateProject.sh

--- tools/CreateProject.sh
+++ tools/CreateProject.sh
@@ -11,8 +11,9 @@ function usage() {
 cat << EOF
 Usage: $0 <project_name> [class_name]
 
-Script create a new directory <project_name> and creates a new model class,
-i.e. the header and source files for the class.
+Creates a new directory <project_name> and a new model class <class_name>,
+i.e. the header and source files for the class. Also a Makefile is created
+for the project.
 
 EOF
 }
@@ -26,13 +27,22 @@ upMYMODEL=`echo $MYMODEL|tr [a-z] [A-Z]`
 
 function m_header() {
 cat << EOF
+// ***************************************************************
+//
+// This file was created using the $0 script
+// for project $MYPROJECT
+// $0 is part of Bayesian Analysis Toolkit (BAT).
+// BAT can be downloaded from http://www.mppmu.mpg.de/bat
+//
+// ***************************************************************
+
 #ifndef __|:UP_MODEL:|__H
 #define __|:UP_MODEL:|__H
 
-#include "BCModel.h"
+#include <BAT/BCModel.h>
 
 // This is a |:Model:| header file.
-// Model source code is located in file |:Project:|/src/|:Model:|.cxx
+// Model source code is located in file |:Project:|/|:Model:|.cxx
 
 // ---------------------------------------------------------
 class |:Model:| : public BCModel
@@ -41,7 +51,7 @@ class |:Model:| : public BCModel
 
 		// Constructors and destructor
 		|:Model:|();
-		|:Model:|(const char* name);
+		|:Model:|(const char * name);
 		~|:Model:|();
 
 		// Methods to overload, see file |:Model:|.cxx
@@ -58,6 +68,15 @@ EOF
 
 function m_sourcecode() {
 cat << EOF
+// ***************************************************************
+//
+// This file was created using the $0 script
+// for project $MYPROJECT
+// $0 is part of Bayesian Analysis Toolkit (BAT).
+// BAT can be downloaded from http://www.mppmu.mpg.de/bat
+//
+// ***************************************************************
+
 #include "|:Model:|.h"
 
 // ---------------------------------------------------------
@@ -67,7 +86,7 @@ cat << EOF
 };
 
 // ---------------------------------------------------------
-|:Model:|::|:Model:|(const char* name) : BCModel(name)
+|:Model:|::|:Model:|(const char * name) : BCModel(name)
 {  // constructor
 	DefineParameters();
 };
@@ -79,11 +98,10 @@ cat << EOF
 // ---------------------------------------------------------
 void |:Model:|::DefineParameters()
 {
-	// add parameters to your model here.
-	//
-	// you can then use them in the methods below by calling the
+	// Add parameters to your model here.
+	// You can then use them in the methods below by calling the
 	// parameters.at(i) or parameters[i], where i is the index
-	// of the parameter. the indices increase from 0 according to the
+	// of the parameter. The indices increase from 0 according to the
 	// order of adding the parameters.
 
 //	this -> AddParameter("par1", 0.0, 1.0);   // index 0
@@ -93,8 +111,8 @@ void |:Model:|::DefineParameters()
 // ---------------------------------------------------------
 double |:Model:|::LogLikelihood(std::vector <double> parameters)
 {
-	// this methods returns the logarithm of the conditional probability
-	// p(data|parameters).
+	// This methods returns the logarithm of the conditional probability
+	// p(data|parameters). This is where you have to define your model.
 
 	double logprob = 0.;
 
@@ -104,8 +122,7 @@ double |:Model:|::LogLikelihood(std::vector <double> parameters)
 // ---------------------------------------------------------
 double |:Model:|::LogAPrioriProbability(std::vector <double> parameters)
 {
-
-	// this method returs the logarithm of the prior probability for the
+	// This method returns the logarithm of the prior probability for the
 	// parameters p(parameters).
 
 	double logprob = 0.;
@@ -122,46 +139,104 @@ EOF
 
 function m_makefile() {
 cat << EOF
+###################################################################
+# This Makefile was created using the $0 script
+# for project $MYPROJECT
+# $0 is part of Bayesian Analysis Toolkit (BAT).
+# BAT can be downloaded from http://www.mppmu.mpg.de/bat
+###################################################################
+#
+# Run 'make' to compile the program and 'make clean' to remove
+# all compiled parts and 'clean' the directory.
+#
+# You might need to adjust the CFLAGS, LIBS, and GLIBS based on
+# the BAT installation on your system. Consult the gmake manual
+# for details.
+#
+###################################################################
+
 # Root variables
 ROOTCFLAGS   := \$(shell root-config --cflags)
 ROOTLIBS     := \$(shell root-config --libs) -lMinuit
 ROOTGLIBS    := \$(shell root-config --glibs)
 
-# Programs
+# compiler and flags
 CXX          = g++
 CXXFLAGS     = -g -Wall -fPIC -Wno-deprecated -O2
 LD           = g++
 LDFLAGS      = -g -O2
 SOFLAGS      = -shared
 
+# standard commands
 RM           = rm -f
 MV           = mv
 ECHO         = echo
 CINT         = rootcint
 
-# Assign or Add variables
+# add ROOT flags
 CXXFLAGS    += \$(ROOTCFLAGS)
-CXXFLAGS    += -I. -I./include -I\$(BATINSTALLDIR)/BAT
 
-LIBS        += \$(ROOTLIBS)  -L\$(BATINSTALLDIR)/lib -lBAT
-GLIBS       += \$(ROOTGLIBS) -L\$(BATINSTALLDIR)/lib -lBAT
+# ----------------------------------------------------------------------
+# The following definitions depend on the setup of the system where
+# the project is being compiled. If BAT is installed in the standard
+# system search path then the lines below are correct and the compilation
+# will work
+CXXFLAGS    += -I. -I./include
+LIBS        += \$(ROOTLIBS)  -lBAT
+GLIBS       += \$(ROOTGLIBS) -lBAT
 
-CXSRCS      = \
+# In case you see following errors during the compilation,
+#
+#   undefined reference to 'Divonne'
+#   undefined reference to 'Suave'
+#   undefined reference to 'Cuhre'
+#   undefined reference to 'Vegas'
+#
+# your version of BAT was installed with the Cuba support and you need
+# to adjust the Makefile by uncommenting the following lines. You might
+# also need to add the path to libcuba.a to the lines below
+# as -L/path/to/cuba/lib
+#
+# LIBS        += -lcuba
+# GLIBS       += -lcuba
+
+# If BAT was installed in a non-standard path (e.g. user home
+# directory) then one can specify the location of the header
+# files here (uncomment following lines and adjust the path in
+# BATINSTALLDIR):
+#
+# BATINSTALLDIR = '/path/to/bat/installation/directory'
+# CXXFLAGS    += -I\$(BATINSTALLDIR)/include
+# LIBS        += -L\$(BATINSTALLDIR)/lib -lBAT
+# GLIBS       += -L\$(BATINSTALLDIR)/lib -lBAT
+
+# List of all classes (models) used in the program
+# Add classes to the end. Baskslash indicates continuation
+# on the next line
+CXXSRCS      = \\
         |:Model:|.cxx
 
-CXXSRCS      = \$(patsubst %.cxx,src/%.cxx,\$(CXSRCS))
-CXXOBJS      = \$(patsubst %.cxx,%.o,\$(CXSRCS))
-EXEOBJS      =
-GARBAGE      = \$(CXXOBJS) \$(EXEOBJS) *.o *~ link.d |:Project:|.exe
+# ----------------------------------------------------------------------
+# don't change lines below unless you know what you're doing
+#
 
+CXXOBJS      = \$(patsubst %.cxx,%.o,\$(CXXSRCS))
+EXEOBJS      =
+MYPROGS     = \\
+        run|:Project:|
+
+GARBAGE      = \$(CXXOBJS) \$(EXEOBJS) *.o *~ link.d \$MYPROGS
+
+
+# targets
 all : project
 
-link.d : \$(patsubst %.cxx,include/%.h,\$(CXSRCS))
+link.d : \$(patsubst %.cxx,%.h,\$(CXXSRCS))
 	\$(CXX) -MM \$(CXXFLAGS) \$(CXXSRCS) > link.d;
 
 include link.d
 
-%.o : src/%.cxx
+%.o : %.cxx
 	\$(CXX) \$(CXXFLAGS) -c \$< -o \$@
 
 clean :
@@ -169,12 +244,10 @@ clean :
 
 project : run|:Project:|.cxx \$(CXXOBJS)
 	\$(CXX) \$(CXXFLAGS) -c \$<
-	\$(CXX) \$(LDFLAGS) \$(LIBS) run|:Project:|.o \\
-		\$(CXXOBJS) -o run|:Project:|.exe
+	\$(CXX) \$(LDFLAGS) \$(LIBS) run|:Project:|.o \$(CXXOBJS) -o run|:Project:|
 
 print :
 	echo compiler  : \$(CXX)
-	echo compiler  : \$(CXSRCS)
 	echo c++ srcs  : \$(CXXSRCS)
 	echo c++ objs  : \$(CXXOBJS)
 	echo c++ flags : \$(CXXFLAGS)
@@ -189,9 +262,17 @@ EOF
 
 function m_main() {
 cat << EOF
-#include "|:Model:|.h"
+// ***************************************************************
+// This file was created using the $0 script
+// for project $MYPROJECT
+// $0 is part of Bayesian Analysis Toolkit (BAT).
+// BAT can be downloaded from http://www.mppmu.mpg.de/bat
+// ***************************************************************
+
 #include <BAT/BCLog.h>
 #include <BAT/BCAux.h>
+
+#include "|:Model:|.h"
 
 int main()
 {
@@ -199,23 +280,30 @@ int main()
 	// set nice style for drawing than the ROOT default
 	BCAux::SetStyle();
 
-	// open log file with default level of logging
-	BCLog::OpenLog("log.txt");
+	// open log file
+//	BCLog::OpenLog("log.txt");
 	BCLog::SetLogLevel(BCLog::detail);
 
 	// create new |:Model:| object
-	|:Model:| * _|:Model:| = new |:Model:|();
+	|:Model:| * m = new |:Model:|();
+
+	BCLog::OutSummary("Test model created");
 
 	// perform your analysis here
-//	_|:Model:| -> Normalize();
-//	_|:Model:| -> MarginalizeAll();
-//	_|:Model:| -> FindMode( _|:Model:| -> GetBestFitParameters() );
-//	_|:Model:| -> PrintAllMarginalized("plots.ps");
-//	_|:Model:| -> CalculatePValue( _|:Model:| -> GetBestFitParameters() );
-//	_|:Model:| -> PrintResults("results.txt");
+//	m -> Normalize();
+//	m -> MarginalizeAll();
+//	m -> FindMode( m -> GetBestFitParameters() );
+//	m -> PrintAllMarginalized("|:Model:|_plots.ps");
+//	m -> CalculatePValue( m -> GetBestFitParameters() );
+//	m -> PrintResults("|:Model:|_results.txt");
 
 	// close log file
-	BCLog::CloseLog();
+//	BCLog::CloseLog();
+
+	delete m;
+
+	BCLog::OutSummary("Test program ran successfully");
+	BCLog::OutSummary("Exiting");
 
 	return 0;
 
@@ -224,12 +312,27 @@ int main()
 EOF
 }
 
-echo "Create project" $MYPROJECT
 mkdir $MYPROJECT
-mkdir $MYPROJECT/src
-mkdir $MYPROJECT/include
 
-m_header | sed -e "s/|\:UP_MODEL\:|/$upMYMODEL/g; s/|\:Model\:|/$MYMODEL/; s/|\:Project\:|/$MYPROJECT/" > $MYPROJECT/include/$MYMODEL.h
-m_sourcecode | sed -e "s/|\:Model\:|/$MYMODEL/g" > $MYPROJECT/src/$MYMODEL.cxx
-m_makefile | sed -e "s/|\:Model\:|/$MYMODEL/g; s/|\:Project\:|/$MYPROJECT/" > $MYPROJECT/Makefile
-m_main | sed -e "s/|\:Model\:|/$MYMODEL/g; s/|\:Project\:|/$MYPROJECT/" > $MYPROJECT/run$MYPROJECT.cxx
+m_header | sed -e "s/|\:UP_MODEL\:|/$upMYMODEL/g; s/|\:Model\:|/$MYMODEL/g; s/|\:Project\:|/$MYPROJECT/g" > $MYPROJECT/$MYMODEL.h
+m_sourcecode | sed -e "s/|\:Model\:|/$MYMODEL/g" > $MYPROJECT/$MYMODEL.cxx
+m_makefile | sed -e "s/|\:Model\:|/$MYMODEL/g; s/|\:Project\:|/$MYPROJECT/g" > $MYPROJECT/Makefile
+m_main | sed -e "s/|\:Model\:|/$MYMODEL/g; s/|\:Project\:|/$MYPROJECT/g" > $MYPROJECT/run$MYPROJECT.cxx
+
+cat << EOF
+--------------------------------------------------------------------------
+The new BAT project was created in the directory '$MYPROJECT'.
+To test the configuration try to compile the project by running 'make'
+inside the directory. In case there are some compilation errors you need
+to adjust the parameters inside the 'Makefile'.
+
+Once the program is compiled successfully, you can run it and it should
+print some basic information on the screen.
+
+Implement your model in file:    $MYMODEL.cxx
+Implement your analysis in file: $MYPROJECT.cxx
+
+Consult BAT webpage for details: http://www.mppmu.mpg.de/bat
+--------------------------------------------------------------------------
+
+EOF
