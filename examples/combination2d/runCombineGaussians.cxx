c6287425e96710f82cdbb9768dd41fd4c00ee2bc
Kevin Kroeninger <kevin.kroeninger@phys.uni-goettingen.de>
2010-07-16 13:55:51 +0000

    Added example for the combination of two-dimensional quantities.

---
+++ examples/combination2d/runCombineGaussians.cxx
@@ -0,0 +1,52 @@
+#include <BAT/BCLog.h>
+#include <BAT/BCAux.h>
+#include <BAT/BCSummaryTool.h>
+
+#include "CombinationModel.h"
+
+int main()
+{
+
+  // set nicer style for drawing than the ROOT default
+  BCAux::SetStyle();
+
+  // open log file
+  BCLog::OpenLog("log.txt");
+  BCLog::SetLogLevel(BCLog::detail);
+
+  // create new CombinationModel object
+  CombinationModel * m = new CombinationModel();
+
+  // create a new summary tool object
+  BCSummaryTool * summary = new BCSummaryTool(m);
+
+  // set precision
+  m->MCMCSetPrecision(BCEngineMCMC::kUltraHigh);
+
+  // marginalize
+  m->MarginalizeAll();
+
+  // find mode
+  m->FindMode( m->GetBestFitParameters() );
+
+  // draw all marginalized distributions into a PostScript file
+  m->PrintAllMarginalized("CombinationModel_plots.ps");
+
+  // print all summary plots
+  summary->PrintParameterPlot("CombinationModel_parameters.ps");
+  summary->PrintCorrelationPlot("CombinationModel_correlation.ps");
+  summary->PrintKnowlegdeUpdatePlot("CombinationModel_update.ps");
+ 
+  // print results of the analysis into a text file
+  m->PrintResults("CombinationModel_results.txt");
+
+  // close log file
+  BCLog::CloseLog();
+
+  delete m;
+  delete summary;
+
+  return 0;
+
+}
+
