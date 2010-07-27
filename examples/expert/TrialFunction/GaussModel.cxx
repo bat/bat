ac2afdbfc46fdb40b5be43076486389cb94c61bc
Kevin Kroeninger <kevin.kroeninger@phys.uni-goettingen.de>
2010-07-27 16:39:11 +0000

    * examples: added first expert example for changing the trial function. * doc/introduction: updated the introduction.

---
+++ examples/expert/TrialFunction/GaussModel.cxx
@@ -0,0 +1,95 @@
+#include "GaussModel.h"
+
+#include <BAT/BCMath.h>
+
+#include <TRandom3.h>
+
+// ---------------------------------------------------------
+GaussModel::GaussModel() : BCModel()
+{  
+  // default constructor
+  DefineParameters();
+};
+
+// ---------------------------------------------------------
+GaussModel::GaussModel(const char * name) : BCModel(name)
+{ 
+  // constructor
+  DefineParameters();
+};
+
+// ---------------------------------------------------------
+GaussModel::~GaussModel()
+// default destructor
+{
+};
+
+// ---------------------------------------------------------
+void GaussModel::DefineParameters()
+{
+  // add parameters x and y
+  AddParameter("x", -10.0, 50.0); // index 0
+  AddParameter("y",  -5.0,  5.0); // index 1
+}
+
+// ---------------------------------------------------------
+double GaussModel::LogLikelihood(std::vector <double> parameters)
+{
+  // assume a simple Gaussian Likelihood with two independent
+  // variables
+  double logprob = 0.;
+
+  double x = parameters.at(0);
+  double y = parameters.at(1);
+
+  // Gaussian Likelihood
+  logprob += BCMath::LogGaus(x, 0.0, 2.0);
+  logprob += BCMath::LogGaus(y, 0.0, 1.0);
+
+  return logprob;
+}
+
+// ---------------------------------------------------------
+double GaussModel::LogAPrioriProbability(std::vector <double> parameters)
+{
+  // assume flat prior in both variables
+  double logprob = 0.;
+
+  double dx = GetParameter(0)->GetRangeWidth(); 
+  double dy = GetParameter(1)->GetRangeWidth(); 
+
+  logprob += log(1./dx); // flat prior for x
+  logprob += log(1./dy); // flat prior for y
+
+  return logprob;
+}
+
+// --------------------------------------------------------
+double GaussModel::MCMCTrialFunctionSingle(int ichain, int iparameter)
+{
+  // no check of range for performance reasons
+
+  // get scale factor from an array of scale factors. the size of the
+  // array is number of chains times number of parameters. 
+  double scale = fMCMCTrialFunctionScaleFactor[ichain * fMCMCNParameters + iparameter];
+
+  // choose trial function by uncommenting any of the lines below
+
+  // Gaussian with fixed width
+  //  return fMCMCRandom->Gaus(0.0, 1.0);
+
+  // Gaussian with adjustable width
+  //  return fMCMCRandom->Gaus(0.0, scale);
+
+  // Breit-Wigner with adjustable width
+  //  return fMCMCRandom->BreitWigner(0.0, scale);
+
+  // Flat function with adjustable width
+  //  return scale * 2. * (0.5 - fMCMCRandom->Uniform());
+
+  // Flat function with fixed width
+  return 0.02 * (0.5 - fMCMCRandom->Uniform());
+
+}
+// ---------------------------------------------------------
+
---
+++ examples/expert/TrialFunction/GaussModel.h
@@ -0,0 +1,27 @@
+#ifndef __GAUSSMODEL__H
+#define __GAUSSMODEL__H
+
+#include <BAT/BCModel.h>
+
+// ---------------------------------------------------------
+class GaussModel : public BCModel
+{
+ public:
+
+  // Constructors and destructor
+  GaussModel();
+  GaussModel(const char * name);
+  ~GaussModel();
+
+  // Methods to overload, see file GaussModel.cxx
+  void DefineParameters();
+  double LogAPrioriProbability(std::vector <double> parameters);
+  double LogLikelihood(std::vector <double> parameters);
+  
+  // overloaded trial function
+  virtual double MCMCTrialFunctionSingle(int ichain, int ipar);
+};
+// ---------------------------------------------------------
+
+#endif
+
