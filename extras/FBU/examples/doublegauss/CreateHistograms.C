688458b1fdd238d64b8ab60257904411a25c4672
Kerim Suruliz <kerim.suruliz@cern.ch>
2012-06-20 18:16:10 +0000

    Added double gaussian example to FBU

---
+++ extras/FBU/examples/doublegauss/CreateHistograms.C
@@ -0,0 +1,164 @@
+void CreateHistograms()
+{
+	// settings 
+
+
+  // convention followed throughout: 1st index: truth, 2nd index: reco
+        
+
+  int nbin = 12;
+
+  TH1F *hist_truth = new TH1F("hist_truth","hist_truth",nbin,0,1);
+  
+  TF1 *f1 = new TF1("f1", "gaus(0) + gaus(3)", 0., 1.);
+  
+  f1->SetParameters(500., 0.25., 0.10, 500, 0.75, 0.10);
+
+  hist_truth->FillRandom("f1",100000);
+
+  hist_truth->Draw();
+
+  TMatrix response(nbin,nbin);
+
+  for (int j=1;j<=nbin;j++)
+    {
+      TF1 *f2 = new TF1("f2", "gaus(0)", 0., 1.);
+  
+      f2->SetParameters(1.,(j-1)*(1.0/nbin)+0.5/nbin, (1.0/nbin));
+      
+      TH1F *h_tmp = new TH1F("h_tmp","h_tmp",nbin,0,1);
+      
+      h_tmp->FillRandom("f2",100000);
+
+      h_tmp->Scale(hist_truth->GetBinContent(j)/h_tmp->Integral());
+
+      for (int i=1;i<=nbin;i++)
+	response(i-1,j-1) = h_tmp->GetBinContent(i); 
+      // sum over first index will give truth number of events in bin
+      // corresponding to efficiency 1
+    }
+
+   for (int j=1;j<=nbin;j++)
+     for (int i=1;i<=nbin;i++)
+	response(i-1,j-1) = 0; 
+
+   for (int j=1;j<=nbin;j++)
+     response(j-1,j-1) = hist_truth->GetBinContent(j);
+
+    for (int i=1;i<=nbin;i++)
+      {
+	for (int j=1;j<=nbin;j++)
+	  cout << response(i-1,j-1) << " ";
+	cout << endl;
+      }
+
+    TMatrix normresponse(nbin,nbin);
+       
+    for (int j=1;j<=nbin;j++)
+      {
+	double eff = 0;
+	
+	for (int i=1;i<=nbin;i++)
+	  {
+	    eff+= response(i-1,j-1);
+	  }
+	
+	cout << "efficiency " << j << " = " << eff/hist_truth->GetBinContent(j) << endl;
+
+
+	for (int i=1; i<=nbin;i++)
+	  normresponse(i-1,j-1) = response(i-1,j-1)/eff;
+	
+      }
+
+      
+
+    TH1F *hist_reco = new TH1F("hist_reco","hist_reco",nbin,0,1);
+
+    for (int i=1;i<=nbin;i++)
+      {
+	double temp = 0;
+
+	for (int j=1;j<=nbin;j++)
+	  {
+	    temp += normresponse(i-1,j-1)*hist_truth->GetBinContent(i);
+	  }
+	
+	hist_reco->SetBinContent(i,temp);
+      }
+
+    hist_reco->SetLineStyle(2);
+
+    hist_reco->Draw("same");
+
+  
+    TH2D *hist_migration = new TH2D("hist_migration","hist_migration",nbin,0,1,nbin,0,1);
+
+    for (int i=1;i<=nbin;i++)
+      {
+	cout << "reco index " << i << " ";
+	for (int j=1;j<=nbin;j++)
+	  {
+	    double tmp = response(i-1,j-1);
+	    cout << tmp << " ";
+	    hist_migration->SetBinContent(i,j,tmp);
+	  }
+	cout << endl;
+      }
+
+    TRandom3 *r = new TRandom3();
+    
+    TH1F *hist_data = new TH1F("hist_data","hist_data",nbin,0,1);
+    
+    for (int i=1;i<=nbin;i++)
+      {
+	//	double newentry = r->PoissonD(hist_reco->GetBinContent(i));
+	
+	double newentry = hist_reco->GetBinContent(i);
+
+	hist_data->SetBinContent(i,newentry);
+      }
+    
+
+    TH1F *hist_bkg = new TH1F("hist_bkg","hist_bkg",nbin,0,1);
+    
+    // write histograms to file 
+    TFile * file = new TFile("histograms.root", "RECREATE");  
+    file -> cd();  
+    
+    hist_migration->Write();  
+    hist_truth->Write();
+    hist_reco->Write();
+    hist_bkg->Write();
+    hist_data->Write();
+    
+    // print migration matrix
+    TCanvas* c1 = new TCanvas("c1", "c1", 900, 300);
+    c1->Divide(3, 1);
+    c1->cd(1);
+    hist_truth->Draw();
+    c1->cd(2);
+    hist_reco->Draw();
+    c1->cd(3);
+    gStyle->SetPalette(1);
+    hist_migration->Draw("COLZ");
+    c1->Print("migration.eps");
+    
+    // print truth, reco and background distributions
+    TCanvas* c2 = new TCanvas("c2", "c2", 900, 300);
+    c2->Divide(3, 1);
+    c2->cd(1);
+    hist_reco->Draw();
+    c2->cd(2);
+    hist_bkg->Draw();
+    c2->cd(3);
+    hist_data->Draw();
+    c2->Print("reco.eps");
+    
+    // close file
+    file->Close();
+    
+    // clean up
+    //	delete hist_migration;
+    //	delete file;
+}
---
+++ extras/FBU/examples/doublegauss/rundoublegaussian.cxx
@@ -0,0 +1,112 @@
+#include <BAT/BCLog.h>
+#include <BAT/BCAux.h>
+#include <BCFBU.h>
+
+#include <TFile.h>
+#include <TH1D.h>
+#include <TH2D.h>
+
+#include "TCanvas.h"
+
+int main()
+{
+
+   // set nicer style for drawing than the ROOT default
+   BCAux::SetStyle();
+
+   // open log file
+   BCLog::OpenLog("log.txt");
+   BCLog::SetLogLevel(BCLog::detail);
+
+	// ---- read histograms from a file ---- // 
+
+	// open file
+	TFile * file = new TFile("histograms.root", "READ");
+
+	// check if file is open
+	if (!file->IsOpen()) {
+		std::cout << "Could not open file. Exit." << std::endl;
+		std::cerr << "Run macro CreateHistograms.C in Root to create the file." << std::endl;
+		return 1;
+	}
+
+	// read histograms
+	TH2D* hist_migration = (TH2D*) file->Get("hist_migration"); 
+	TH1D* hist_truth     = (TH1D*) file->Get("hist_truth"); 
+	TH1D* hist_bkg       = (TH1D*) file->Get("hist_bkg");
+	TH1D* hist_data      = (TH1D*) file->Get("hist_data");
+
+	// check if histograms are really there 
+	if (!hist_migration) {
+		std::cout << "Could not find migration matrix. Run CreateHistograms before running the example. Exit." << std::endl;
+		return(1);
+	}
+
+	if (!hist_truth) {
+		std::cout << "Could not find truth matrix. Run CreateHistograms before running the example. Exit." << std::endl;
+		return(1);
+	}
+
+	if (!hist_bkg) {
+		std::cout << "Could not find bkg matrix. Run CreateHistograms before running the example. Exit." << std::endl;
+		return(1);
+	}
+
+	if (!hist_data) {
+		std::cout << "Could not find data matrix. Run CreateHistograms before running the example. Exit." << std::endl;
+		return(1);
+	}
+
+	// create new BCFBU object
+	BCFBU* m = new BCFBU();
+
+	// prepare response matrix and calculate efficiency
+	m->PrepareResponseMatrix(hist_migration, hist_truth, hist_bkg);
+ 
+	// set data
+	m->SetDataHistogram(hist_data);
+	
+	// print
+	m->PrintResponseMatrix("response_matrix.eps");
+	m->PrintMigrationMatrix("migration_matrix.eps");
+	m->PrintEfficiencyHistogram("efficiency.eps");
+	m->PrintTruthHistogram("truth.eps");
+	m->PrintDataHistogram("data.eps");
+
+
+	m->DefineParameters();
+
+	m->MarginalizeAll();
+
+	TH1D *unfolded = (TH1D*) m->GetUnfoldedResult();
+
+	TCanvas *bla = new TCanvas("bla","bla",600,600);
+	//	 bla->SetLogy(); 
+	
+	hist_truth->Draw();
+	unfolded->SetMarkerColor(kBlue);
+	unfolded->SetLineColor(kBlue);
+	unfolded->Draw("E same");
+	
+	bla->SaveAs("unfolded.eps"); 
+
+
+	// close log file
+	BCLog::CloseLog();
+
+	BCLog::OutSummary("Test program ran successfully");
+	BCLog::OutSummary("Exiting");
+
+	// clean up
+	delete hist_migration;
+	delete hist_truth;
+	delete hist_bkg;
+	delete hist_data;
+	delete m;
+  delete file;
+
+	// no error 
+	return 0;
+
+}
+
