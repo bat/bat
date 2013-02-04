650516675d83e40d89479d4c93d656e6d1a06e93
Kevin Kroeninger <kevin.kroeninger@phys.uni-goettingen.de>
2013-02-04 21:05:30 +0000

    Quick fix.

--- src/BCH1D.cxx
+++ src/BCH1D.cxx
@@ -812,7 +812,7 @@ void BCH1D::Draw(std::string options, std::vector<double> intervals)
    marker_mode->SetMarkerColor(GetColor(4));
    marker_mode->SetMarkerSize(1.5);
 
-   TMarker* marker_mean = new TMarker(GetMean(), 0.55*ymaxhist, 20);
+   TMarker* marker_mean = new TMarker(GetMean(), 0.55*ymaxhist, 32);
    marker_mean->SetMarkerColor(GetColor(4));
    marker_mean->SetMarkerSize(1.5);
 
@@ -861,7 +861,7 @@ void BCH1D::Draw(std::string options, std::vector<double> intervals)
       marker_mean->Draw();
       TLegendEntry* le = legend->AddEntry(arrow_std, "mean and standard deviation", "PL");
       le->SetLineColor(GetColor(4));
-      le->SetMarkerStyle(20);
+      le->SetMarkerStyle(32);
       le->SetMarkerSize(1.5);
       le->SetMarkerColor(GetColor(4));
    }
