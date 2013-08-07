d2a0f307905a398b354020cd6fd7ad6e34f201ad
Kevin Kroeninger <kevin.kroeninger@phys.uni-goettingen.de>
2013-08-07 15:01:33 +0000

    Clean-up of reference counting example.

--- examples/advanced/referencecounting/ReferenceCounting.cxx
+++ examples/advanced/referencecounting/ReferenceCounting.cxx
@@ -1,9 +1,3 @@
-// ***************************************************************
-// This file was created using the CreateProject.sh script.
-// CreateProject.sh is part of Bayesian Analysis Toolkit (BAT).
-// BAT can be downloaded from http://www.mppmu.mpg.de/bat
-// ***************************************************************
-
 #include "ReferenceCounting.h"
 
 #include <BAT/BCMath.h>
@@ -17,16 +11,16 @@
 
 // ---------------------------------------------------------
 ReferenceCounting::ReferenceCounting() : BCModel()
-																			 , fEvalOption(kHistogram)
-																			 , fNObs(0)
-																			 , fEps(1e-6)
-																			 , fAlpha(10)
-																			 , fBeta(5)
-																			 , logs(0)
-																			 , maxn(0)
-																			 , maxk(10000)
-																			 , fHistPriorS(0)
-																			 , fHistPriorB(0)
+                                       , fEvalOption(kHistogram)
+                                       , fNObs(0)
+                                       , fEps(1e-6)
+                                       , fAlpha(10)
+                                       , fBeta(5)
+                                       , logs(0)
+                                       , maxn(0)
+                                       , maxk(10000)
+                                       , fHistPriorS(0)
+                                       , fHistPriorB(0)
 {
    // default constructor
    DefineParameters();
@@ -34,16 +28,16 @@ ReferenceCounting::ReferenceCounting() : BCModel()
 
 // ---------------------------------------------------------
 ReferenceCounting::ReferenceCounting(const char * name) : BCModel(name)
-																												, fEvalOption(kHistogram)
-																												, fNObs(0)
-																												, fEps(1e-6)
-																												, fAlpha(10)
-																												, fBeta(5)
-																												, logs(0)
-																												, maxn(0)
-																												, maxk(10000)
-																												, fHistPriorS(0)
-																												, fHistPriorB(0)
+                                                        , fEvalOption(kHistogram)
+                                                        , fNObs(0)
+                                                        , fEps(1e-6)
+                                                        , fAlpha(10)
+                                                        , fBeta(5)
+                                                        , logs(0)
+                                                        , maxn(0)
+                                                        , maxk(10000)
+                                                        , fHistPriorS(0)
+                                                        , fHistPriorB(0)
 {
    // constructor
    DefineParameters();
@@ -51,59 +45,50 @@ ReferenceCounting::ReferenceCounting(const char * name) : BCModel(name)
 
 // ---------------------------------------------------------
 ReferenceCounting::~ReferenceCounting()
-   // default destructor
+// default destructor
 {
-	if (fHistPriorS)
-		delete fHistPriorS;
+   if (fHistPriorS)
+      delete fHistPriorS;
 
-	if (fHistPriorB)
-		delete fHistPriorB; 
+   if (fHistPriorB)
+      delete fHistPriorB; 
 }
 
 // ---------------------------------------------------------
 void ReferenceCounting::SetAlphaBeta(double alpha, double beta)
 {
-	fAlpha = alpha; 
-	fBeta  = beta;
+   fAlpha = alpha; 
+   fBeta  = beta;
 
-	// clear vectors
-	helper_a.clear();
-	helper_b.clear();
-	helper_c.clear();
+   // clear vectors
+   helper_a.clear();
+   helper_b.clear();
+   helper_c.clear();
 }
 
 // ---------------------------------------------------------
 void ReferenceCounting::DefineParameters()
 {
-   // Add parameters to your model here.
-   // You can then use them in the methods below by calling the
-   // parameters.at(i) or parameters[i], where i is the index
-   // of the parameter. The indices increase from 0 according to the
-   // order of adding the parameters.
-
-	AddParameter("s", 0, 50.); // index 0
-	AddParameter("b", 0, 50.); // index 1
+   AddParameter("s", 0, 50.); // index 0
+   AddParameter("b", 0, 50.); // index 1
 }
 
 // ---------------------------------------------------------
 double ReferenceCounting::LogLikelihood(const std::vector<double> &parameters)
 {
-   // This methods returns the logarithm of the conditional probability
-   // p(data|parameters). This is where you have to define your model.
+   if (!fHistPriorS)
+      FillPriorS();
 
-	if (!fHistPriorS)
-		FillPriorS();
-
-	if (!fHistPriorB)
-		FillPriorB();
+   if (!fHistPriorB)
+      FillPriorB();
 
    double logprob = 0.;
 
    double s = parameters.at(0);
    double b = parameters.at(1);
-	 double nu = s+b;
+   double nu = s+b;
 
-	 logprob += BCMath::LogPoisson( double(fNObs), nu);
+   logprob += BCMath::LogPoisson( double(fNObs), nu);
 
    return logprob;
 }
@@ -111,28 +96,21 @@ double ReferenceCounting::LogLikelihood(const std::vector<double> &parameters)
 // ---------------------------------------------------------
 double ReferenceCounting::LogAPrioriProbability(const std::vector<double> &parameters)
 {
-   // This method returns the logarithm of the prior probability for the
-   // parameters p(parameters).
-
    double logprob = 0.;
 
    double s = parameters.at(0);
    double b = parameters.at(1);
 
-	 if (fEvalOption == kAnalytic) {
-		 logprob += log(RefPriorS(s));
-		 logprob += log(ConjPriorPoisson(b, fAlpha, fBeta));
-	 }
-	 else if (fEvalOption == kHistogram) {
-		 int bins = fHistPriorS->FindBin(s);
-		 int binb = fHistPriorB->FindBin(b);
-		 logprob += log( fHistPriorS->GetBinContent(bins) );
-		 logprob += log( fHistPriorB->GetBinContent(binb) );
-	 }
-	 else if (fEvalOption == kHistogram) {
-		 // debugKK: Needs to be implemented
-		 // ...
-	 }
+   if (fEvalOption == kAnalytic) {
+      logprob += log(RefPriorS(s));
+      logprob += log(ConjPriorPoisson(b, fAlpha, fBeta));
+   }
+   else if (fEvalOption == kHistogram) {
+      int bins = fHistPriorS->FindBin(s);
+      int binb = fHistPriorB->FindBin(b);
+      logprob += log( fHistPriorS->GetBinContent(bins) );
+      logprob += log( fHistPriorB->GetBinContent(binb) );
+   }
 
    return logprob;
 }
@@ -140,58 +118,58 @@ double ReferenceCounting::LogAPrioriProbability(const std::vector<double> &param
 // ---------------------------------------------------------
 double ReferenceCounting::ConjPriorPoisson(double nu, double alpha, double beta)
 {
-	return TMath::GammaDist(nu, alpha, 0., 1/beta);
+   return TMath::GammaDist(nu, alpha, 0., 1/beta);
 }
 
 // ---------------------------------------------------------
 double ReferenceCounting::RefPriorS(double s)
 {
-	return sqrtI(s) / sqrtI(0.);
+   return sqrtI(s) / sqrtI(0.);
 }
 
 // ---------------------------------------------------------
 double ReferenceCounting::sqrtI(double s)
 {
-	double sum = 0;
+   double sum = 0;
 
-	double inc = 1.;
+   double inc = 1.;
 
-	for (int n=0; n<=maxk; ++n) {
-		double t1 = f(s, n);
-		double t2 = f(s, n+1);
-		double dsum = t1*t1/t2;
-		sum += dsum;
-		inc = fabs( dsum/sum );
+   for (int n=0; n<=maxk; ++n) {
+      double t1 = f(s, n);
+      double t2 = f(s, n+1);
+      double dsum = t1*t1/t2;
+      sum += dsum;
+      inc = fabs( dsum/sum );
 
-		// check if sum does not change by more than predefined precision
-		if (inc < fEps)
-			break;
-	}
+      // check if sum does not change by more than predefined precision
+      if (inc < fEps)
+         break;
+   }
 
-	return sqrt( fabs( TMath::Power(fBeta/(1+fBeta), fAlpha) * exp(-s) * sum - 1 ) );
+   return sqrt( fabs( TMath::Power(fBeta/(1+fBeta), fAlpha) * exp(-s) * sum - 1 ) );
 }
 
 // ---------------------------------------------------------
 double ReferenceCounting::f(double s, int k)
 {
-	if (s > 0)
-		logs = log(s);
-	else {
-		if (k==0) 
-			return 1;
-		else 
-			return (fAlpha+k-1)/(k*(1+fBeta)) * f(0., k-1);
-	}
+   if (s > 0)
+      logs = log(s);
+   else {
+      if (k==0) 
+         return 1;
+      else 
+         return (fAlpha+k-1)/(k*(1+fBeta)) * f(0., k-1);
+   }
 
-	double ftemp = 0;
+   double ftemp = 0;
 
-	extend_abc(k+2);
+   extend_abc(k+2);
 
-	for (int n = 0; n <= k; ++n) {
-		ftemp += df(s, k, n);
-	}
+   for (int n = 0; n <= k; ++n) {
+      ftemp += df(s, k, n);
+   }
 
-	return ftemp;
+   return ftemp;
 }
 
 // ---------------------------------------------------------
@@ -203,83 +181,83 @@ double ReferenceCounting::df(double s, int k, int n)
 // ---------------------------------------------------------
 double ReferenceCounting::logdf(double s, int k, int n)
 {
-	double logdf = 0;
+   double logdf = 0;
 
-	logdf = helper_a[n] + (k-n) * logs - helper_b[n] - helper_c[k-n];
+   logdf = helper_a[n] + (k-n) * logs - helper_b[n] - helper_c[k-n];
 
-	return logdf;
+   return logdf;
 }
 
 // ---------------------------------------------------------
 void ReferenceCounting::extend_abc(int n)
 {
-	if (n <= maxn)
-		return;
+   if (n <= maxn)
+      return;
 	
-	int nfrom = maxn;
+   int nfrom = maxn;
 	
-	if (nfrom == 0) {
-		helper_a.push_back( log(1) );
-		helper_b.push_back( 0. );
-    helper_c.push_back( 0. );
-		++nfrom;
-	}
-	if (nfrom == 1) {
-		helper_a.push_back( log(fAlpha) );
-		helper_b.push_back( log( 1. + fBeta) );
-		helper_c.push_back( helper_c[0] );
-		++nfrom;
-	}
+   if (nfrom == 0) {
+      helper_a.push_back( log(1) );
+      helper_b.push_back( 0. );
+      helper_c.push_back( 0. );
+      ++nfrom;
+   }
+   if (nfrom == 1) {
+      helper_a.push_back( log(fAlpha) );
+      helper_b.push_back( log( 1. + fBeta) );
+      helper_c.push_back( helper_c[0] );
+      ++nfrom;
+   }
 	
-	if (maxn > 1)
-		nfrom++;
+   if (maxn > 1)
+      nfrom++;
 
-	for (int i = nfrom; i <= n; ++i) {
-		double temp_a = helper_a[i-1] + log( 1. + (fAlpha-1.)/double(i)); 
-		helper_a.push_back(temp_a);
-		helper_b.push_back( int(i) * helper_b[1] );
-		helper_c.push_back( log( double(i) ) + helper_c[i-1]); 
-	}
+   for (int i = nfrom; i <= n; ++i) {
+      double temp_a = helper_a[i-1] + log( 1. + (fAlpha-1.)/double(i)); 
+      helper_a.push_back(temp_a);
+      helper_b.push_back( int(i) * helper_b[1] );
+      helper_c.push_back( log( double(i) ) + helper_c[i-1]); 
+   }
 
-	maxn = int( helper_a.size() - 1);
+   maxn = int( helper_a.size() - 1);
 }
 
 // ---------------------------------------------------------
 void ReferenceCounting::FillPriorS()
 {
-	// remove old prior histogram
-	if (fHistPriorS)
-		delete fHistPriorS;
+   // remove old prior histogram
+   if (fHistPriorS)
+      delete fHistPriorS;
 
-	// create new histogram
-	BCParameter * p = GetParameter(0);
-	fHistPriorS = new TH1D("hist_prior_s", ";s;p(s)", p->GetNbins(), p->GetLowerLimit(), p->GetUpperLimit());
+   // create new histogram
+   BCParameter * p = GetParameter(0);
+   fHistPriorS = new TH1D("hist_prior_s", ";s;p(s)", p->GetNbins(), p->GetLowerLimit(), p->GetUpperLimit());
 	
-	// fill histogram
-	for (unsigned i = 1; i <= p->GetNbins(); ++i){
-		double s = fHistPriorS->GetBinCenter(i);
-		double p = RefPriorS(s);
-		fHistPriorS->SetBinContent(i, p);
-	}
+   // fill histogram
+   for (unsigned i = 1; i <= p->GetNbins(); ++i){
+      double s = fHistPriorS->GetBinCenter(i);
+      double p = RefPriorS(s);
+      fHistPriorS->SetBinContent(i, p);
+   }
 }
 
 // ---------------------------------------------------------
 void ReferenceCounting::FillPriorB()
 {
-	// remove old prior histogram
-	if (fHistPriorB)
-		delete fHistPriorB;
+   // remove old prior histogram
+   if (fHistPriorB)
+      delete fHistPriorB;
 
-	// create new histogram
-	BCParameter * p = GetParameter(1);
-	fHistPriorB = new TH1D("hist_prior_b", ";b;p(b)", p->GetNbins(), p->GetLowerLimit(), p->GetUpperLimit());
+   // create new histogram
+   BCParameter * p = GetParameter(1);
+   fHistPriorB = new TH1D("hist_prior_b", ";b;p(b)", p->GetNbins(), p->GetLowerLimit(), p->GetUpperLimit());
 	
-	// fill histogram
-	for (unsigned i = 1; i <= p->GetNbins(); ++i){
-		double b = fHistPriorB->GetBinCenter(i);
-		double p = ConjPriorPoisson(b, fAlpha, fBeta);
-		fHistPriorB->SetBinContent(i, p);
-	}
+   // fill histogram
+   for (unsigned i = 1; i <= p->GetNbins(); ++i){
+      double b = fHistPriorB->GetBinCenter(i);
+      double p = ConjPriorPoisson(b, fAlpha, fBeta);
+      fHistPriorB->SetBinContent(i, p);
+   }
 }
 
 // ---------------------------------------------------------
--- examples/advanced/referencecounting/ReferenceCounting.h
+++ examples/advanced/referencecounting/ReferenceCounting.h
@@ -1,9 +1,3 @@
-// ***************************************************************
-// This file was created using the CreateProject.sh script.
-// CreateProject.sh is part of Bayesian Analysis Toolkit (BAT).
-// BAT can be downloaded from http://www.mppmu.mpg.de/bat
-// ***************************************************************
-
 #ifndef __BAT__REFERENCECOUNTING__H
 #define __BAT__REFERENCECOUNTING__H
 
@@ -17,72 +11,72 @@ class ReferenceCounting : public BCModel
 {
 public:
 
-   // An enumerator for the prior evaluation options.
-   // kAnalytic: use analytic approximation
-   // kHistogram: fill a histogram with the prior before running
-   // kExpo: use an exponential approximation for the signal prior
-   enum EvalOption{ kAnalytic, kHistogram, kExpo };
+        // An enumerator for the prior evaluation options.
+        // kAnalytic: use analytic approximation
+        // kHistogram: fill a histogram with the prior before running
+        // kExpo: use an exponential approximation for the signal prior
+        enum EvalOption{ kAnalytic, kHistogram, kExpo };
 
-   // Constructors and destructor
-   ReferenceCounting();
-   ReferenceCounting(const char * name);
-   ~ReferenceCounting();
+        // Constructors and destructor
+        ReferenceCounting();
+        ReferenceCounting(const char * name);
+        ~ReferenceCounting();
 
-   // Methods to overload, see file ReferenceCounting.cxx
-   void DefineParameters();
-   double LogAPrioriProbability(const std::vector<double> &parameters);
-   double LogLikelihood(const std::vector<double> &parameters);
-   // void MCMCIterationInterface();
+        // Methods to overload, see file ReferenceCounting.cxx
+        void DefineParameters();
+        double LogAPrioriProbability(const std::vector<double> &parameters);
+        double LogLikelihood(const std::vector<double> &parameters);
+        // void MCMCIterationInterface();
 
-   // set option of how to evaluate the reference prior
-   void SetPriorEvalOption(ReferenceCounting::EvalOption option)
-   { fEvalOption = option; };
+        // set option of how to evaluate the reference prior
+        void SetPriorEvalOption(ReferenceCounting::EvalOption option)
+        { fEvalOption = option; };
 
-   void SetAlphaBeta(double alpha, double beta);
-   void SetNObs(int n)
-   { fNObs = n; };
+        void SetAlphaBeta(double alpha, double beta);
+        void SetNObs(int n)
+        { fNObs = n; };
 
-   // the reference prior for the signal strength
-   double RefPriorS(double s);
+        // the reference prior for the signal strength
+        double RefPriorS(double s);
 
-   // the conjugate prior for the background
-   double ConjPriorPoisson(double nu, double alpha, double beta);
+        // the conjugate prior for the background
+        double ConjPriorPoisson(double nu, double alpha, double beta);
 
-   // option of how to evaluate the signal prior
-   EvalOption fEvalOption;
+        // option of how to evaluate the signal prior
+        EvalOption fEvalOption;
 
-   // helper functions for calculating priors
-   double sqrtI(double s);
+        // helper functions for calculating priors
+        double sqrtI(double s);
 
-   void extend_abc(int n);
-   double f(double s, int k);
-   double df(double s, int k, int n);
-   double logdf(double s, int k, int n);
+        void extend_abc(int n);
+        double f(double s, int k);
+        double df(double s, int k, int n);
+        double logdf(double s, int k, int n);
 
-   // helper functions for histogram
-   void FillPriorS();
-   void FillPriorB();
+        // helper functions for histogram
+        void FillPriorS();
+        void FillPriorB();
 
-   // number of observed events
-   int fNObs;
+        // number of observed events
+        int fNObs;
 
-   // the precision of the reference prior sum
-   double fEps;
+        // the precision of the reference prior sum
+        double fEps;
 
-   // helper variables
-   double fAlpha; 			// the shape variable for the conjugate priors
-   double fBeta;	  		// the shape variable for the conjugate priors
-   double logs; // lograrithm of s
-   int maxn; // upper limit of the helpers
-   int maxk; // maximum number of sums
+        // helper variables
+        double fAlpha; 			// the shape variable for the conjugate priors
+        double fBeta;	  		// the shape variable for the conjugate priors
+        double logs; // lograrithm of s
+        int maxn; // upper limit of the helpers
+        int maxk; // maximum number of sums
 
-   std::vector<double> helper_a;
-   std::vector<double> helper_b;
-   std::vector<double> helper_c;
+        std::vector<double> helper_a;
+        std::vector<double> helper_b;
+        std::vector<double> helper_c;
 
-   // the histogrammed priors
-   TH1D* fHistPriorS;
-   TH1D* fHistPriorB;
+        // the histogrammed priors
+        TH1D* fHistPriorS;
+        TH1D* fHistPriorB;
 
 
 };
