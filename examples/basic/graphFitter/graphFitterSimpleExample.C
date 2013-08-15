a1c859bcea3030de63ed2c1fd496fba860d30ebc
Kevin Kroeninger <kevin.kroeninger@phys.uni-goettingen.de>
2013-08-15 10:15:29 +0000

    Clean up of GraphFitter example.

--- examples/basic/graphFitter/graphFitterSimpleExample.C
+++ examples/basic/graphFitter/graphFitterSimpleExample.C
@@ -64,72 +64,70 @@ const double sigma  =  5.0;
 // ---------------------------------------------------------
 void graphFitterSimpleExample()
 {
-	// set nice style for plot drawing
-	BCAux::SetStyle();
+   // set nicer style for drawing than the ROOT default
+   BCAux::SetStyle();
 
-	// set global message printing level to detail
-	BCLog::SetLogLevel(BCLog::detail);
+   // open log file
+   BCLog::OpenLog("log.txt");
+   BCLog::SetLogLevel(BCLog::detail);
 
-	// create data
-	TGraphErrors * graph = CreateGraph(10, 1000);
+   // create data
+   TGraphErrors * graph = CreateGraph(10, 1000);
 
-	// define a fit function
-	TF1 * f1 = new TF1("f1", "[0] + [1]*x", 0.0, 100.0);
+   // define a fit function
+   TF1 * f1 = new TF1("f1", "[0] + [1]*x", 0.0, 100.0);
 
-	// allowed range has to be defined for every parameter
-	f1->SetParLimits(0, -20.0, 30.0);
-	f1->SetParLimits(1,   0.5,  1.5);
+   // allowed range has to be defined for every parameter
+   f1->SetParLimits(0, -20.0, 30.0);
+   f1->SetParLimits(1,   0.5,  1.5);
 
-	// create a new graph fitter
-	BCGraphFitter * gf = new BCGraphFitter(graph, f1);
+   // create a new graph fitter
+   BCGraphFitter * gf = new BCGraphFitter(graph, f1);
 
-	// set options for MCMC
-	// set number of iterations of Markov Chain after convergence
-	// by default 5 chains are run in paralell, i.e. total number
-	// of iterations will be 5 times what is set below
-	gf->MCMCSetNIterationsRun(100000);
+   // set precision
+   gf->MCMCSetPrecision(BCEngineMCMC::kMedium);
 
-	// perform the fit
-	gf->Fit();
+   // perform the fit
+   gf->Fit();
 
-	// print data and the fit
-	TCanvas * c1 = new TCanvas("c1");
-	gf->DrawFit("", true);
-	c1->Print("fit.pdf");
+   // print data and the fit
+   TCanvas * c1 = new TCanvas("c1");
+   gf->DrawFit("", true);
+   c1->Print("fit.pdf");
 
-	// print marginalized distributions
-	gf->PrintAllMarginalized("distributions.pdf");
+   // print marginalized distributions
+   gf->PrintAllMarginalized("distributions.pdf");
 
-	// print results
-	gf->PrintResults("results.txt");
+   // print results
+   gf->PrintResults("results.txt");
 }
 
 // ---------------------------------------------------------
 TGraphErrors * CreateGraph(int n, int seed)
 {
-	// initialize random number generator
-	gRandom = new TRandom3(seed);
+   // initialize random number generator
+   gRandom = new TRandom3(seed);
 
-	// define arrays
-	double * x  = new double[n];
-	double * y  = new double[n];
-	double * ey = new double[n];
+   // define arrays
+   double * x  = new double[n];
+   double * y  = new double[n];
+   double * ey = new double[n];
 
-	// define x and y-values and the uncertainties on y
-	for (int i = 0; i < n; ++i)
-	{
-		x[i] = 100. / double(n) * double(i+0.5);
-		y[i] = gRandom->Gaus(offset + slope*x[i], sigma);
-		ey[i] = sigma;
-	}
+   // define x and y-values and the uncertainties on y
+   for (int i = 0; i < n; ++i)
+   {
+      x[i] = 100. / double(n) * double(i+0.5);
+      y[i] = gRandom->Gaus(offset + slope*x[i], sigma);
+      ey[i] = sigma;
+   }
 
-	// create new graph
-	TGraphErrors * graph = new TGraphErrors(n, x, y, 0, ey);
-	graph->SetMarkerStyle(20);
-	graph->SetMarkerSize(1.5);
+   // create new graph
+   TGraphErrors * graph = new TGraphErrors(n, x, y, 0, ey);
+   graph->SetMarkerStyle(20);
+   graph->SetMarkerSize(1.5);
 
-	// return the graph
-	return graph;
+   // return the graph
+   return graph;
 }
 
 // ---------------------------------------------------------
