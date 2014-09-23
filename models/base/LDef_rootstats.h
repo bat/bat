97f0bdb8670b87a600974b5c35afbf5423538c3c
Oliver Schulz <oschulz@mpp.mpg.de>
2014-09-23 22:03:26 +0200

    [build] Made build compatible with ROOT-6 (and ROOT-5)
    New LinkDef-based dictionary generation scheme with automatic
    rootmap-file generation. Uses rootcint for ROOT-5 and rootcling for
    ROOT-6

---
+++ models/base/LDef_rootstats.h
@@ -0,0 +1,18 @@
+#ifndef LinkDef_h
+#define LinkDef_h
+
+#ifdef __CINT__
+
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+
+#pragma link C++ class BCRooInterface-;
+#pragma link C++ class RooStats::BATCalculator-;
+
+
+#endif
+
+#endif // LinkDef_h
---
+++ models/mtf/LinkDef.h
@@ -0,0 +1,24 @@
+#ifndef LinkDef_h
+#define LinkDef_h
+
+#ifdef __CINT__
+
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+
+#pragma link C++ class BCMTFAnalysisFacility;
+#pragma link C++ class BCMTFChannel;
+#pragma link C++ class BCMTFComparisonTool;
+#pragma link C++ class BCMTF;
+#pragma link C++ class BCMTFProcess;
+#pragma link C++ class BCMTFSystematic;
+#pragma link C++ class BCMTFSystematicVariation;
+#pragma link C++ class BCMTFTemplate;
+
+
+#endif
+
+#endif // LinkDef_h
