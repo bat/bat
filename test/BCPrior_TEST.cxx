2db6d020745904fa461325467c5c33652389440b
Daniel Greenwald <daniel.greenwald@tum.de>
2015-03-25 19:59:10 +0100

    All priors tested in BCPrior_TEST. All pass (but BCSplitGaussianPrior does not test variance <- TODO).

--- test/BCPrior_TEST.cxx
+++ test/BCPrior_TEST.cxx
@@ -21,6 +21,9 @@
 #include <BAT/BCTF1Prior.h>
 #include <BAT/BCTH1Prior.h>
 
+#include <TF1.h>
+#include <TH1D.h>
+
 using namespace test;
 
 class BCPriorTest :
@@ -33,37 +36,37 @@ public:
     }
 
     // check the mode and integral of a prior distribution
-    void TestPriorDistribution(BCPrior * prior, double xmin, double xmax, double mode, double integral) const
+    void TestPriorDistribution(BCPrior * prior, double xmin, double xmax, double mode, double integral, double eps=1.e-5) const
     {
         // mode
         if (std::isfinite(mode))
-            TEST_CHECK_NEARLY_EQUAL( prior->GetMode(xmin,xmax), mode, 1.e-5 );
+            TEST_CHECK_NEARLY_EQUAL( prior->GetMode(xmin,xmax), mode, eps );
         else
             TEST_CHECK_EQUAL( prior->GetMode(xmin,xmax), mode );
         
         if (std::isfinite(integral))
-            TEST_CHECK_NEARLY_EQUAL( prior->GetIntegral(xmin,xmax), integral, 1.e-5);
+            TEST_CHECK_NEARLY_EQUAL( prior->GetIntegral(xmin,xmax), integral, eps);
         else
             TEST_CHECK_EQUAL( prior->GetIntegral(xmin,xmax), integral);
     }
 
     // check the mode, integral, mean, and variance of a prior distribution
     // ignores variance check if negative
-    void TestPriorDistribution(BCPrior * prior, double xmin, double xmax, double mode, double integral, double mean, double variance=-1) const
+    void TestPriorDistribution(BCPrior * prior, double xmin, double xmax, double mode, double integral, double mean, double variance, double eps=1.e-5) const
     {
         // check mode & integral
         TestPriorDistribution(prior,xmin,xmax,mode,integral);
 
         // mean
         if (std::isfinite(mean))
-            TEST_CHECK_NEARLY_EQUAL( prior->GetMean(xmin,xmax), mean, 1.e-5 );
+            TEST_CHECK_NEARLY_EQUAL( prior->GetMean(xmin,xmax), mean, eps );
         else
             TEST_CHECK_EQUAL( prior->GetMean(xmin,xmax), mean );
         
         // variance
         if (variance >= 0) {
             if (std::isfinite(variance))
-                TEST_CHECK_NEARLY_EQUAL( prior->GetVariance(xmin,xmax), variance, 1.e-5 );
+                TEST_CHECK_NEARLY_EQUAL( prior->GetVariance(xmin,xmax), variance, eps );
             else
                 TEST_CHECK_EQUAL( prior->GetVariance(xmin,xmax), variance );
         }
@@ -72,31 +75,31 @@ public:
 
     // Test the implementation of the prior against the implementation
     // of the mode, integral, and moment (upto maxn'th moment)
-    void TestPriorImplementation(BCPrior * prior, double xmin, double xmax, unsigned maxn) const
+    void TestPriorImplementation(BCPrior * prior, double xmin, double xmax, unsigned maxn=2, double eps=1.e-5) const
     {
         // BCPrior::Functions use ROOT routines to calculate from implementation of prior
 
         // mode
         TEST_CHECK_NEARLY_EQUAL( prior->GetMode(xmin,xmax),
                                  prior->BCPrior::GetMode(xmin,xmax),
-                                 1.e-5 ); 
+                                 eps ); 
         
         // integral
         TEST_CHECK_NEARLY_EQUAL( prior->GetIntegral(xmin,xmax),
                                  prior->BCPrior::GetIntegral(xmin,xmax),
-                                 1.e-5 ); 
+                                 eps ); 
         
         // raw moments
         for (unsigned n=1; n<=maxn; ++n)
             TEST_CHECK_NEARLY_EQUAL( prior->GetRawMoment(n,xmin,xmax),
                                      prior->BCPrior::GetRawMoment(n,xmin,xmax),
-                                     1.e-5 );
+                                     eps );
 
         // central moments
         for (unsigned n=1; n<=maxn; ++n)
             TEST_CHECK_NEARLY_EQUAL( prior->GetCentralMoment(n,xmin,xmax),
                                      prior->BCPrior::GetCentralMoment(n,xmin,xmax),
-                                     1.e-5 );
+                                     eps );
     }
 
     virtual void run() const
@@ -160,19 +163,56 @@ public:
         TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(1.5),  -2.30523 , 1.e-5); // at mean
         TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(6.5),  -2.80523 , 1.e-5); // at mean+sigma_above
         TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(-4.5), -4.30523 , 1.e-5); // at mean-2*sigma_below
-        //                            xmin,    xmax,    mode, I,       mean, var
-        TestPriorDistribution( prior, neg_inf, pos_inf, 1.5,  1,       3.09577); // [ -inf , +inf ]
-        TestPriorDistribution( prior, -4.5,    6.5,     1.5,  0.78462, 1.76119); // [  fin, fin  ]
-        TestPriorDistribution( prior, 6.5,     pos_inf, 6.5,  0.19832, 9.12568); // [  fin, +inf ] above mean
+        //                            xmin,    xmax,    mode, I,       mean,    var
+        TestPriorDistribution( prior, neg_inf, pos_inf, 1.5,  1,       3.09577, -1); // [ -inf , +inf ]
+        TestPriorDistribution( prior, -4.5,    6.5,     1.5,  0.78462, 1.76119, -1); // [  fin, fin  ]
+        TestPriorDistribution( prior, 6.5,     pos_inf, 6.5,  0.19832, 9.12568, -1); // [  fin, +inf ] above mean
+        delete prior;
+        std::cout << "PASS" << std::endl;
+
+        // TF1LogPrior
+        std::cout << "Testing BCTF1LogPrior ... " << std::flush;
+        // TF1 of ln(Normal)
+        prior = new BCTF1LogPrior("-0.5*((x-[0])/[1])^2 - log([1]) - 0.5*log(2*pi)",-10,10);
+        dynamic_cast<BCTF1LogPrior*>(prior) -> GetLogFunction() -> SetParameters(1.5,3);
+        TestPriorImplementation( prior, -10, 10, 2);
+        TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(1.5),  -2.01755 , 1.e-5); // at mean
+        TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(4.5),  -2.51755 , 1.e-5); // at mean+sigma
+        TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(-4.5), -4.01755 , 1.e-5); // at mean-2*sigma
+        //                            xmin,    xmax,    mode, integral, mean,    var,     
+        TestPriorDistribution( prior, -1.5,    4.5,     1.5,  0.68269,  1.5,     2.62013);  // [  fin , fin  ]
         delete prior;
         std::cout << "PASS" << std::endl;
         
-        
-        // TF1LogPrior
 
         // TF1 Prior
+        std::cout << "Testing BCTF1Prior ... " << std::flush;
+        // TF1 of Normal
+        prior = new BCTF1Prior("exp(-0.5*((x-[0])/[1])^2)/sqrt(2*pi)/[1]",-10,10);
+        prior -> GetFunction() -> SetParameters(1.5,3);
+        TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(1.5),  -2.01755 , 1.e-5); // at mean
+        TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(4.5),  -2.51755 , 1.e-5); // at mean+sigma
+        TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(-4.5), -4.01755 , 1.e-5); // at mean-2*sigma
+        //                            xmin,    xmax,    mode, integral, mean,    var,     
+        TestPriorDistribution( prior, -1.5,    4.5,     1.5,  0.68269,  1.5,     2.62013);  // [  fin , fin  ]
+        delete prior;
+        std::cout << "PASS" << std::endl;
 
         // TH1Prior
+        std::cout << "Testing BCTH1Prior ... " << std::flush;
+        TH1D * h1_prior = new TH1D("h1_prior","",100,-5,5);
+        h1_prior -> FillRandom("gaus",1000000);
+        prior = new BCTH1Prior(h1_prior,true);
+        TestPriorImplementation( prior, -10, 10, 2, 1.e-2);
+        TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(0),  -0.91894 , 5.e-2); // at mean
+        TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(1),  -1.41894 , 5.e-2); // at mean+sigma
+        TEST_CHECK_NEARLY_EQUAL( prior->GetLogPrior(-2), -2.91894 , 5.e-2); // at mean-2*sigma
+        TEST_CHECK_NEARLY_EQUAL( prior->GetMode(-5,5), 0, 1.e-1);
+        TEST_CHECK_EQUAL( prior->GetIntegral(-5,5), 1 );
+        TEST_CHECK_NEARLY_EQUAL( prior->GetMean(-5,5), 0, 1.e-2);
+        TEST_CHECK_NEARLY_EQUAL( prior->GetVariance(-5,5), 1, 1.e-2);
+        delete prior;
+        std::cout << "PASS" << std::endl;
 
     }
 
