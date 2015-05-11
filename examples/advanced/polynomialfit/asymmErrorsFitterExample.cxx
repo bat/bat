385c3f7b257f11f5165eeedcbd07fc02920f2970
Daniel Greenwald <daniel.greenwald@tum.de>
2015-05-11 16:38:26 -0400

    [examples/advanced/polynomialfit] Automated most tasks, now user can try adding additional polynomials to see how it works.

--- examples/advanced/polynomialfit/asymmErrorsFitterExample.cxx
+++ examples/advanced/polynomialfit/asymmErrorsFitterExample.cxx
@@ -7,7 +7,7 @@
 
 #include <TCanvas.h>
 #include <TGraphAsymmErrors.h>
-#include <TH2D.h>
+#include <TH2.h>
 #include <TLegend.h>
 
 // ---------------------------------------------------------
@@ -18,20 +18,20 @@ int main()
     BCAux::SetStyle();
 
     // open log file with default level of logging
-    BCLog::OpenLog("log.txt", BCLog::detail, BCLog::detail);
+    BCLog::OpenLog("log.txt", BCLog::detail, BCLog::summary);
 
     // 1st order polynomial
-    PolAsymm m1("Pol1");
-    m1.AddParameter("p0", -0.2, 1.2);
-    m1.AddParameter("p1", 0.015, 0.045);
+    PolAsymm m1("1st-Order Polynomial");
+    m1.AddParameter("p0", -0.1, 2.5);
+    m1.AddParameter("p1", -0.1, 0.1);
     m1.SetPriorConstantAll();
     m1.PrintSummary();
 
     // 2nd order polynomial
-    PolAsymm m2("Pol2");
-    m2.AddParameter("p0", 0., 2.2);
-    m2.AddParameter("p1", -0.1, 0.05);
-    m2.AddParameter("p2", 0.0, 0.001);
+    PolAsymm m2("2nd-Order Polynomial");
+    m2.AddParameter("p0", -0.1, 2.5);
+    m2.AddParameter("p1", -0.1, 0.1);
+    m2.AddParameter("p2", -1e-4, 15e-4);
     m2.SetPriorConstantAll();
     m2.PrintSummary();
 
@@ -39,8 +39,8 @@ int main()
     BCModelManager mgr;
 
     // add models to it, each with 50% of the a priori probability
-    mgr.AddModel(&m1, 0.5);
-    mgr.AddModel(&m2, 0.5);
+    mgr.AddModel(&m1, 1. / 2);
+    mgr.AddModel(&m2, 1. / 2);
 
     // create a new data set and fill it with data from a textfile
     // with four columns: x, y, y_error_low, y_error_high
@@ -50,37 +50,33 @@ int main()
         return -1;
     }
 
-    data.Dump();
+    // in this example the data points are not chosen arbitrarily but
+    // they have fixed values on the x-axis, i.e, 5, 15, ... . also, the
+    // resolution is fixed. So these data axes are fixed:
+    data.Fix(0); // fixes x-values
+    data.Fix(2); // fixes resolution
+    data.Fix(3); // fixes resolution
+
+    // adjust y (1) for 2.*sigma uncertainties
+    // from y_error_low (2) and y_error_high (3)
+    data.AdjustBoundForUncertainties(1, 2., 2, 3);
+
+    data.PrintSummary();
 
     // assigns the data set to the model manager.
     mgr.SetDataSet(&data);
 
-    // normalize all models and calculate the Bayes factors
-    //	mgr.SetIntegrationMethod(BCIntegrate::kIntMonteCarlo);
+    // normalize all models
     mgr.Integrate();
 
-    // one can manually set the data bounds with the following three lines
-    // but BAT will automatically know the data bounds
-    // mgr.GetDataSet()->SetBounds(0, 0.0, 100.0); // possible x-values
-    // mgr.GetDataSet()->SetBounds(1, 0.0, 5.0);   // possible y-values
-    // mgr.GetDataSet()->SetBounds(2, 0.2, 5.2);   // possible sigmas
-
-    // in this example the data points are not chosen arbitrarily but
-    // they have fixed values on the x-axis, i.e, 5, 15, ... . also, the
-    // resolution is fixed. in general this can be solved by fixing the
-    // data axes.
-    mgr.GetDataSet()->Fix(0); // fixes x-values
-    mgr.GetDataSet()->Fix(2); // fixes resolution
-    mgr.GetDataSet()->Fix(3); // fixes resolution
-
-    // during the marginalization, the error propagation can be done
-    // thus, the x- and y-indices of the data values have to be set.
-    m1.SetFitFunctionIndices(0, 1);
-    m2.SetFitFunctionIndices(0, 1);
-
-    // turns on the calculation of the error band
-    m1.SetFillErrorBand();
-    m2.SetFillErrorBand();
+    // for each model (as a BCFitter)
+    for (unsigned i = 0; i < mgr.GetNModels(); ++i) {
+        BCFitter* m = dynamic_cast<BCFitter*>(mgr.GetModel(i));
+        // tell model which data axes correspond to x and y axes
+        m->SetFitFunctionIndices(0, 1);
+        // turn on calculaton of error band
+        m->SetFillErrorBand();
+    }
 
     // set Metropolis algorithm for both models
     mgr.SetMarginalizationMethod(BCIntegrate::kMargMetropolis);
@@ -88,23 +84,19 @@ int main()
     // set precision
     mgr.MCMCSetPrecision(BCEngineMCMC::kMedium);
 
-    // marginalizes the probability density with respect to all
-    // parameters subsequently for all models
+    // the manager will marginalize all models
     mgr.MarginalizeAll();
 
-    // the MarginalizeAll method will also perform a mode finding
-    // but we run minuit on top of that starting in the mode
-    // found by MCMC to get more precise result
-    for (unsigned int i = 0; i < mgr.GetNModels(); i++)
-        mgr.GetModel(i)->FindMode(mgr.GetModel(i)->GetGlobalMode());
-
-    // write marginalized distributions to a root file
-    m1.WriteMarginalizedDistributions("Pol1.root", "RECREATE");
-    m2.WriteMarginalizedDistributions("Pol2.root", "RECREATE");
-
-    // print all marginalized distributions to a postscript file
-    m1.PrintAllMarginalized("Pol1-marg.pdf");
-    m2.PrintAllMarginalized("Pol2-marg.pdf");
+    // For each model
+    for (unsigned i = 0; i < mgr.GetNModels(); ++i) {
+        BCModel* m = mgr.GetModel(i);
+        // find mode, starting from global mode found by marginalizing
+        m->FindMode(m->GetGlobalMode());
+        // write distrubutions to .root file
+        m->WriteMarginalizedDistributions(m->GetSafeName() + "_plots.root", "RECREATE");
+        // write distributions to .pdf file
+        m->PrintAllMarginalized(m->GetSafeName() + "_plots.pdf");
+    }
 
     // print the model comparison summary to the log
     mgr.PrintModelComparisonSummary();
@@ -112,75 +104,52 @@ int main()
     // print data with best fit result from both models
     TCanvas* c1 = new TCanvas();
 
-    // defines a data graph with asymmetric errors
-    TGraphAsymmErrors* gdata = new TGraphAsymmErrors();
+    // Get data set as TGraphAsymmErrors, with x-axis errors set 0
+    TGraphAsymmErrors* gdata = data.GetGraph(0, 1, -1, -1, 2, 3);
     gdata->SetMarkerStyle(20);
     gdata->SetMarkerColor(kBlack);
 
-    // sets the points of the graph to the data points read in
-    // previously. loops over all entries.
-    for (unsigned i = 0; i < mgr.GetDataSet()->GetNDataPoints(); i++) {
-        gdata->SetPoint(i, mgr.GetDataSet()->GetDataPoint(i)->GetValue(0), mgr.GetDataSet()->GetDataPoint(i)->GetValue(1));
-        gdata->SetPointEYlow(i, mgr.GetDataSet()->GetDataPoint(i)->GetValue(2));
-        gdata->SetPointEYhigh(i, mgr.GetDataSet()->GetDataPoint(i)->GetValue(3));
-    }
-
     // defines a histogram for the axes and draws it.
-    TH2D* haxes = new TH2D("haxes", ";x;y", 1, 0., 100., 1, 0., 5.);
+    TH2* haxes = data.CreateH2("haxes", ";x;y", 0, 1);
     haxes->SetStats(false);
     haxes->Draw();
 
-    // draw data
-    gdata->Draw("p same");
-
-    // gets the best fit function graphs and draws them
-    TGraph* gm1_best = m1.GetFitFunctionGraph();
-    gm1_best->SetLineColor(kBlue);
-
-    TGraph* gm2_best = m2.GetFitFunctionGraph();
-    gm2_best->SetLineColor(kRed);
-
-    gm1_best->Draw("c same");
-    gm2_best->Draw("c same");
-
-    // defines and draw a legend.
+    // define the legend
     TLegend* leg = new TLegend(0.2, 0.60, 0.6, 0.8);
     leg->SetFillColor(kWhite);
     leg->SetBorderSize(0);
-    leg->AddEntry(gdata, "Data", "p");
-    leg->AddEntry(gm1_best, "1st order polynomial", "l");
-    leg->AddEntry(gm2_best, "2nd order polynomial", "l");
-    leg->Draw("same");
+
+    // add data to legend
+    leg->AddEntry(gdata, "Data", "PE");
+
+    int Colors[3] = {kBlue, kRed, kGreen};
+
+    // draw best fit functions
+    // for each model (as BCFitter)
+    for (unsigned i = 0; i < mgr.GetNModels(); ++i) {
+        BCFitter* m = dynamic_cast<BCFitter*>(mgr.GetModel(i));
+
+        TGraph* g_err = m->GetErrorBandGraph(0.16, 0.84);
+        g_err->SetFillColor(Colors[i] - 7);
+        g_err->SetFillStyle(3002);
+        g_err->Draw("f");
+
+        TGraph* g_best = m->GetFitFunctionGraph();
+        g_best->SetLineColor(Colors[i]);
+        g_best->SetFillColor(Colors[i] - 7);
+        g_best->SetFillStyle(3002);
+
+        g_best->Draw("c same");
+        leg->AddEntry(g_best, mgr.GetModel(i)->GetName().data(), "LF");
+    }
+
+    gdata->Draw("p same");
+
+    // draw the legend
+    leg->Draw();
 
     // print the canvas to a .pdf file
     c1->Print("data_allmodels.pdf");
-
-    // defines a new canvas
-    TCanvas* c2 = new TCanvas("c2", "", 800, 500);
-    c2->Divide(2, 1);
-
-    // draw best fit for model 1 with errorband
-    c2->cd(1);
-    haxes->Draw();
-    // draw central 68% errorband
-    m1.GetErrorBandGraph(.16, .84)->Draw("f");
-    gdata->Draw("p same");
-    gm1_best->SetLineColor(kRed);
-    gm1_best->Draw("c same");
-    gPad->RedrawAxis();
-
-    // draw best fit for model 2 with errorband
-    c2->cd(2);
-    haxes->Draw();
-    // draw central 68% errorband
-    m2.GetErrorBandGraph(.16, .84)->Draw("f");
-    gdata->Draw("p same");
-    gm2_best->SetLineColor(kRed);
-    gm2_best->Draw("c same");
-    gPad->RedrawAxis();
-
-    // print the canvas to a .pdf file
-    c2->Print("data_errorbands.pdf");
 
     // close log file
     BCLog::CloseLog();
