63874b3fff172c1e0b7a9282b17f467ff6ebf77b
Daniel Greenwald <daniel.greenwald@tum.de>
2015-07-07 19:58:36 +0200

    [LinkDef.h] Formatting

--- src/LinkDef.h
+++ src/LinkDef.h
@@ -3,11 +3,9 @@
 
 #ifdef __CINT__
 
-
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-
 
 #pragma link C++ class BCAux-;
 #pragma link C++ class BCCauchyPrior-;
