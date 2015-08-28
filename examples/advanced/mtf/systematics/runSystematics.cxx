23838b289ae09d72df9be1c1cec05860ae00eca6
Daniel Greenwald <daniel.greenwald@tum.de>
2015-08-28 17:54:59 +0200

    [examples/advanced/mtf/systematics] modernization!

--- examples/advanced/mtf/systematics/runSystematics.cxx
+++ examples/advanced/mtf/systematics/runSystematics.cxx
@@ -1,5 +1,7 @@
-#include <BAT/BCLog.h>
 #include <BAT/BCAux.h>
+#include <BAT/BCGaussianPrior.h>
+#include <BAT/BCLog.h>
+#include <BAT/BCParameter.h>
 
 #include <BAT/BCMTFAnalysisFacility.h>
 #include <BAT/BCMTF.h>
@@ -27,27 +29,36 @@ int main()
     TFile* file = TFile::Open(fname.c_str(), "READ");
 
     // check if file is open
-    if (!file->IsOpen()) {
+    if (!file || file->IsZombie()) {
         BCLog::OutError(Form("Could not open file %s.", fname.c_str()));
         BCLog::OutError("Run macro CreateHistograms.C in Root to create the file.");
         return 1;
     }
 
     // read template histograms
-    TH1D hist_signal      = *((TH1D*) file->Get("hist_sgn"));    // signal template
-    TH1D hist_background  = *((TH1D*) file->Get("hist_bkg"));    // background template
+    TH1D* hist_signal      = (TH1D*) file->Get("hist_sgn");    // signal template
+    TH1D* hist_background  = (TH1D*) file->Get("hist_bkg");    // background template
     // read data histograms
-    TH1D hist_data1       = *((TH1D*) file->Get("hist_data1"));  // data for channel 1
-    TH1D hist_data2       = *((TH1D*) file->Get("hist_data2"));  // data for channel 2
+    TH1D* hist_data1       = (TH1D*) file->Get("hist_data1");  // data for channel 1
+    TH1D* hist_data2       = (TH1D*) file->Get("hist_data2");  // data for channel 2
     // read systematic histograms
-    TH1D hist_syst1_bkg_1 = *((TH1D*) file->Get("hist_syst1_bkg_1"));
-    TH1D hist_syst1_sgn_1 = *((TH1D*) file->Get("hist_syst1_sgn_1"));
-    TH1D hist_syst1_bkg_2 = *((TH1D*) file->Get("hist_syst1_bkg_2"));
-    TH1D hist_syst1_sgn_2 = *((TH1D*) file->Get("hist_syst1_sgn_2"));
-    TH1D hist_syst2_bkg_1 = *((TH1D*) file->Get("hist_syst2_bkg_1"));
-    TH1D hist_syst2_sgn_1 = *((TH1D*) file->Get("hist_syst2_sgn_1"));
-    TH1D hist_syst2_bkg_2 = *((TH1D*) file->Get("hist_syst2_bkg_2"));
-    TH1D hist_syst2_sgn_2 = *((TH1D*) file->Get("hist_syst2_sgn_2"));
+    TH1D* hist_syst1_bkg_1 = (TH1D*) file->Get("hist_syst1_bkg_1");
+    TH1D* hist_syst1_sgn_1 = (TH1D*) file->Get("hist_syst1_sgn_1");
+    TH1D* hist_syst1_bkg_2 = (TH1D*) file->Get("hist_syst1_bkg_2");
+    TH1D* hist_syst1_sgn_2 = (TH1D*) file->Get("hist_syst1_sgn_2");
+    TH1D* hist_syst2_bkg_1 = (TH1D*) file->Get("hist_syst2_bkg_1");
+    TH1D* hist_syst2_sgn_1 = (TH1D*) file->Get("hist_syst2_sgn_1");
+    TH1D* hist_syst2_bkg_2 = (TH1D*) file->Get("hist_syst2_bkg_2");
+    TH1D* hist_syst2_sgn_2 = (TH1D*) file->Get("hist_syst2_sgn_2");
+
+    if (!hist_signal || !hist_background || !hist_data1 || !hist_data2
+            || !hist_syst1_bkg_1 || !hist_syst1_sgn_1
+            || !hist_syst1_bkg_2 || !hist_syst1_sgn_2
+            || !hist_syst2_bkg_1 || !hist_syst2_sgn_1
+            || !hist_syst2_bkg_2 || !hist_syst2_sgn_2) {
+        BCLog::OutError("Could not open data histograms.");
+        return 1;
+    }
 
     // ---- perform fitting ---- //
 
@@ -75,32 +86,32 @@ int main()
     m.AddSystematic("systematic2", -5., 5.);
 
     // set data
-    m.SetData("channel1", hist_data1);
-    m.SetData("channel2", hist_data2);
+    m.SetData("channel1", *hist_data1);
+    m.SetData("channel2", *hist_data2);
 
     // set template histograms
-    m.SetTemplate("channel1", "background", hist_background, 1.0);
-    m.SetTemplate("channel1", "signal",     hist_signal,     1.0);
+    m.SetTemplate("channel1", "background", *hist_background, 1.0);
+    m.SetTemplate("channel1", "signal",     *hist_signal,     1.0);
 
-    m.SetTemplate("channel2", "background", hist_background, 1.0);
-    m.SetTemplate("channel2", "signal",     hist_signal,     0.5);
+    m.SetTemplate("channel2", "background", *hist_background, 1.0);
+    m.SetTemplate("channel2", "signal",     *hist_signal,     0.5);
 
     // set systematic histograms
     m.SetSystematicVariation("channel1", "background", "systematic1", 0.1, 0.1);
-    m.SetSystematicVariation("channel1", "signal", "systematic1", hist_syst1_sgn_1, hist_syst1_sgn_1);
+    m.SetSystematicVariation("channel1", "signal", "systematic1", *hist_syst1_sgn_1, *hist_syst1_sgn_1);
     m.SetSystematicVariation("channel2", "background", "systematic1", 0.1, 0.1);
-    m.SetSystematicVariation("channel2", "signal", "systematic1", hist_syst1_sgn_2, hist_syst1_sgn_2);
+    m.SetSystematicVariation("channel2", "signal", "systematic1", *hist_syst1_sgn_2, *hist_syst1_sgn_2);
 
-    m.SetSystematicVariation("channel1", "background", "systematic2", hist_syst2_bkg_1, hist_syst2_bkg_1);
-    m.SetSystematicVariation("channel1", "signal", "systematic2", hist_syst2_sgn_1, hist_syst2_sgn_1);
-    m.SetSystematicVariation("channel2", "background", "systematic2", hist_syst2_bkg_2, hist_syst2_bkg_2);
-    m.SetSystematicVariation("channel2", "signal", "systematic2", hist_syst2_sgn_2, hist_syst2_sgn_2);
+    m.SetSystematicVariation("channel1", "background", "systematic2", *hist_syst2_bkg_1, *hist_syst2_bkg_1);
+    m.SetSystematicVariation("channel1", "signal", "systematic2", *hist_syst2_sgn_1, *hist_syst2_sgn_1);
+    m.SetSystematicVariation("channel2", "background", "systematic2", *hist_syst2_bkg_2, *hist_syst2_bkg_2);
+    m.SetSystematicVariation("channel2", "signal", "systematic2", *hist_syst2_sgn_2, *hist_syst2_sgn_2);
 
     // set priors
-    m.SetPriorGauss("background", 300., 30.);
-    m.SetPriorConstant("signal");
-    m.SetPriorGauss("systematic1", 0., 1.);
-    m.SetPriorGauss("systematic2", 0., 1.);
+    m.GetParameter("signal").SetPriorConstant();
+    m.GetParameter("background").SetPrior(new BCGaussianPrior(300., 30.));
+    m.GetParameter("systematic1").SetPrior(new BCGaussianPrior(0., 1.));
+    m.GetParameter("systematic2").SetPrior(new BCGaussianPrior(0., 1.));
 
     // run MCMC
     m.MarginalizeAll();
@@ -123,11 +134,11 @@ int main()
     // print templates and stacks
     for (int i = 0; i < m.GetNChannels(); ++i) {
         BCMTFChannel* channel = m.GetChannel(i);
-        channel->PrintTemplates(Form("%s_templates.pdf", channel->GetName().c_str()));
+        channel->PrintTemplates(channel->GetSafeName() + "_templates.pdf");
         channel->PrintTemplate(0, Form("background_%i.pdf", i));
         channel->PrintTemplate(1, Form("signal_%i.pdf", i));
 
-        m.PrintStack(i, m.GetGlobalMode(), Form("%s_stack.pdf", channel->GetName().c_str()));
+        m.PrintStack(i, m.GetGlobalMode(), channel->GetSafeName() + "_stack.pdf");
     }
 
     // ---- perform single systematic analysis ---- //
