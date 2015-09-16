b2270d76ea5d7a7b7d7c3a049d5319f1aafac105
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2015-09-16 13:36:43 +0200

    Extend copyright notice to 2015

--- BAT/BCEmptyModel.h
+++ BAT/BCEmptyModel.h
@@ -10,7 +10,7 @@
  */
 
 /*
- * Copyright (C) 2007-2014, the BAT core developer team
+ * Copyright (C) 2007-2015, the BAT core developer team
  * All rights reserved.
  *
  * For the licensing terms see doc/COPYING.
