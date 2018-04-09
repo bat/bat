da4a4268f7d3402fe521b985202a55e273a1521c
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2018-04-09 13:57:17 +0200

    [src] refactored BCObservable, BCObservableSet
    fixes #250

--- src/BCObservable.cxx
+++ src/BCObservable.cxx
@@ -8,47 +8,18 @@
 
 #include "BCObservable.h"
 
-#include <string>
-
-
 // ---------------------------------------------------------
 BCObservable::BCObservable()
     : BCVariable()
-    , fObservableValue(new double)
+    , fObservableValue(0)
 {
     fPrefix = "Observable";
-}
-
-// ---------------------------------------------------------
-BCObservable::BCObservable(const BCObservable& other, bool share_pointer)
-    : BCVariable(other)
-    , fObservableValue(share_pointer ? other.fObservableValue : new double)
-{
 }
 
 // ---------------------------------------------------------
 BCObservable::BCObservable(const std::string& name, double lowerlimit, double upperlimit, const std::string& latexname, const std::string& unitstring)
     :	BCVariable(name, lowerlimit, upperlimit, latexname, unitstring)
-    , fObservableValue(new double)
+    , fObservableValue(0)
 {
     fPrefix = "Observable";
-}
-
-// ---------------------------------------------------------
-BCObservable::~BCObservable()
-{
-}
-
-// ---------------------------------------------------------
-BCObservable& BCObservable::operator=(BCObservable other)
-{
-    swap(*this, other);
-    return *this;
-}
-
-// ---------------------------------------------------------
-void swap(BCObservable& A, BCObservable& B)
-{
-    std::swap(static_cast<BCVariable&>(A), static_cast<BCVariable&>(B));
-    std::swap(A.fObservableValue, B.fObservableValue);
 }
