529b1c4272415cf489c8add995adcb5ad7e29b66
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2018-04-10 12:02:52 +0200

    [tools] Require zero compiler warnings on travis

--- tools/test-bat.sh
+++ tools/test-bat.sh
@@ -4,6 +4,6 @@
 
 ./autogen.sh
 ./configure --with-cuba=download --enable-roostats --enable-parallel
-make -j`nprocs` distcheck VERBOSE=1
+make -j`nprocs` distcheck VERBOSE=1 CXXFLAGS='-Wall -Wextra -pedantic -Werror'
 
 echo "OK"
