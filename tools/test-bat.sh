ce8d13b35054b57812856db847473d08fd04bc47
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2018-04-11 12:15:07 +0200

    [tools] test-bat with CXXFLAGS set during configure

--- tools/test-bat.sh
+++ tools/test-bat.sh
@@ -3,7 +3,7 @@
 # Standard BAT build/test script (Linux)
 
 ./autogen.sh
-./configure --with-cuba=download --enable-roostats --enable-parallel
-make -j`nprocs` distcheck VERBOSE=1 CXXFLAGS='-Wall -Wextra -pedantic -Werror'
+./configure --with-cuba=download --enable-roostats --enable-parallel CXXFLAGS='-Wall -Wextra -pedantic -Werror'
+make -j`nprocs` distcheck VERBOSE=1
 
 echo "OK"
