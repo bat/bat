3094a39bc2121fecd59bf53b7513f35bd493e2ad
Daniel Greenwald <daniel.greenwald@tum.de>
2018-05-07 19:51:32 +0200

    Added continuation example to examples/basic/rootOutput

---
+++ examples/basic/rootOutput/continueMCMC.cxx
@@ -0,0 +1,40 @@
+#include <BAT/BCLog.h>
+#include <BAT/BCAux.h>
+
+#include "GaussModel.h"
+
+#include <string>
+
+int main()
+{
+
+    // open log file
+    BCLog::OpenLog("log.txt", BCLog::detail, BCLog::detail);
+
+    // create new GaussModel object
+    GaussModel m("Continue Gauss Model");
+
+    // load in previous running from file
+    m.PrepareToContinueMarginalization("GaussModel.root");
+
+    // write out new samples to a new file
+    m.WriteMarkovChain(m.GetSafeName() + ".root", "RECREATE");
+
+    // continue marginalization
+    m.MarginalizeAll(BCIntegrate::kMargMetropolis);
+
+    // find the new mode
+    m.FindMode(m.GetBestFitParameters());
+
+    // print new marginalization plots
+    m.PrintAllMarginalized(m.GetSafeName() + "_plots.pdf");
+
+    // print results of the analysis to the log
+    m.PrintSummary();
+
+    // close log file
+    BCLog::CloseLog();
+
+    return 0;
+}
+
