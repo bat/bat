cb878d7156184d16cd6ba5aab21f32dc6e5f1d1d
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2018-05-18 13:10:10 +0200

    [manual] Finish mcmc

--- doc/newcommands.js
+++ doc/newcommands.js
@@ -2,8 +2,11 @@ MathJax.Hub.Config({
     TeX: {
         Macros: {
             cond: ["{\\,|\\,}"],
+            diag: ["{\\mbox{diag}}"],
+            matsig: ["{\\boldsymbol{\\Sigma}}"],
             rmdx: ["{\\mbox{d}#1\\,}",1],
             scath: ["{\\theta}"],
+            vecmu: ["{\\boldsymbol{\\mu}}"],
             vecth: ["{\\boldsymbol{\\theta}}"],
             order: ["{\\mathcal{O}(#1)}",1],
         }
